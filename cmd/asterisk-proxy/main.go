b37dd6e2682fdfade5241f040a378cbf74f996b8
sungtae kim <pchero21@gmail.com>
2020-03-21 21:15:05 +0100

    init

--- cmd/asterisk-proxy/main.go
+++ cmd/asterisk-proxy/main.go
@@ -22,30 +22,33 @@ var rabbitAddr = flag.String("rabbit_addr", "amqp://guest:guest@localhost:5672",
 var rabbitQueue = flag.String("rabbit_queue", "asterisk_ari", "rabbitmq queue name.")
 
 // create message buffer
-var messages = make(chan []byte, 1024000)
+var chMessages = make(chan []byte, 1024000)
 
 func main() {
-	flag.Parse()
-	// log.SetFlags(0)
-
-	log.SetFormatter(&log.JSONFormatter{})
-
-	log.Info("asterisk-proxy started.")
+	initiate()
 
 	// asterisk ari message receiver
-	go recvEventFromAst()
+	go handleARI()
 
 	// push the message into rabbitmq
-	go publishEvent()
+	go handleRabbitMQ()
 
 	forever := make(chan bool)
 	<-forever
 }
 
-// recvEventFromAst connects Asterisk's ARI websocket and receives ARI events.
-func recvEventFromAst() {
+// initiate initiates asterisk_proxy
+func initiate() {
+	flag.Parse()
+	log.SetFormatter(&log.JSONFormatter{})
+	log.Info("asterisk-proxy started.")
+}
+
+// connectARI connects to Asterisk's ARI websocket.
+// return *websocket.Conn must be closed after use.
+func connectARI(account, subscribe, application string) (*websocket.Conn, error) {
 	// create url query
-	rawquery := fmt.Sprintf("api_key=%s&subscribeAll=%s&app=%s", *ariAccount, *ariSubscribeAll, *ariApplication)
+	rawquery := fmt.Sprintf("api_key=%s&subscribeAll=%s&app=%s", account, subscribe, application)
 
 	u := url.URL{
 		Scheme:   "ws",
@@ -53,56 +56,111 @@ func recvEventFromAst() {
 		Path:     "/ari/events",
 		RawQuery: rawquery,
 	}
-	log.Printf("Dial string: %s", u.String())
+	log.Debugf("Connect to Asterisk ARI. dial string: %s", u.String())
 
+	// connect
+	conn, _, err := websocket.DefaultDialer.Dial(u.String(), nil)
+	if err != nil {
+		return nil, err
+	}
+
+	return conn, nil
+}
+
+// handleARIEevnt reads the event from the websocket and writes to the channel.
+func recvARIEvent(c *websocket.Conn) error {
+	// receive ARI events
+	msgType, msgStr, err := c.ReadMessage()
+	if err != nil {
+		log.Errorf("Could not read message. msgType: %d, err: %v", msgType, err)
+		return err
+	}
+	chMessages <- msgStr
+
+	return nil
+}
+
+// handleARI handles ARI events and ARI connection
+func handleARI() {
+	// connect to Asterisk ARI
 	for {
-		// connect
-		c, _, err := websocket.DefaultDialer.Dial(u.String(), nil)
+		conn, err := connectARI(*ariAccount, *ariSubscribeAll, *ariApplication)
 		if err != nil {
-			log.Println("Could not connect to server. err: ", err)
+			log.Errorf("Could not connect to Asterisk ARI. err: %v", err)
+			time.Sleep(time.Second * 1)
 
-			// sleep for every second
-			time.Sleep(1 * time.Second)
 			continue
 		}
-		defer c.Close()
+		defer conn.Close()
 
-		// receiver
+		// receive ARI events
 		for {
-			msgType, msgStr, err := c.ReadMessage()
-			if err != nil {
-				log.Printf("Could not read message. msgType: %d, err: %s", msgType, err)
+			if err := recvARIEvent(conn); err != nil {
+				log.Errorf("Could not recv the ARI event. err: %v", err)
 				break
 			}
-
-			// insert msg into queue
-			messages <- msgStr
 		}
 
-		// sleep 1 second for reconnect
 		time.Sleep(1 * time.Second)
 	}
 }
 
-// push the message into rabbitmq
-func publishEvent() {
+// connectRabbitMQ connects to the gvien rabbitMQ address.
+// returned *amqp.Connection must be closed after use.
+func connectRabbitMQ(addr string) (*amqp.Connection, error) {
+	// connect to rabbit mq
+	conn, err := amqp.Dial(addr)
+	if err != nil {
+		return nil, err
+	}
+
+	return conn, nil
+}
+
+// publishMSG watchs messages channel and send it to given rabbitMQ channel/queue.
+func publishMSG(ch *amqp.Channel, q amqp.Queue) error {
+	// message sending
 	for {
-		// connect to rabbit mq
-		conn, err := amqp.Dial(*rabbitAddr)
+		select {
+		case msg := <-chMessages:
+			// message send
+			err := ch.Publish(
+				"",     // excahnge
+				q.Name, // routing key
+				false,  // madatory
+				false,
+				amqp.Publishing{
+					DeliveryMode: amqp.Persistent,
+					ContentType:  "text/plain",
+					Body:         msg,
+				},
+			)
+			if err != nil {
+				return err
+			}
+		}
+	}
+}
+
+// handleRabbitMQ handles rabbitMQ connection and message delivery.
+func handleRabbitMQ() {
+	for {
+		// connect to rabbitmq
+		conn, err := connectRabbitMQ(*rabbitAddr)
 		if err != nil {
-			log.Printf("Could not connect to RabbitMQ. err: %s", err)
+			log.Errorf("Could not connect to Rabbitmq. addr: %s, err: %v", *rabbitAddr, err)
 
 			time.Sleep(1 * time.Second)
 			continue
 		}
 		defer conn.Close()
 
-		// declare channel
+		// create channel
 		ch, err := conn.Channel()
 		if err != nil {
-			log.Printf("Could not declare a channel. err: %s", err)
+			log.Errorf("Could not create a channel for RabbitMQ send. err: %v", err)
 
-			time.Sleep(1 * time.Second)
+			time.Sleep(time.Second * 1)
 			continue
 		}
 		defer ch.Close()
@@ -117,29 +175,18 @@ func publishEvent() {
 			nil,          // arguments
 		)
 		if err != nil {
-			log.Printf("Could not declare a queue. err: %s", err)
+			log.Errorf("Could not declare a queue. err: %v", err)
 
-			time.Sleep(1 * time.Second)
+			time.Sleep(time.Second * 1)
 			continue
 		}
 
-		// message sending
-		for {
-			select {
-			case msg := <-messages:
-				// message send
-				err = ch.Publish(
-					"",     // excahnge
-					q.Name, // routing key
-					false,  // madatory
-					false,
-					amqp.Publishing{
-						DeliveryMode: amqp.Persistent,
-						ContentType:  "text/plain",
-						Body:         msg,
-					},
-				)
-			}
+		// publish the message
+		if err := publishMSG(ch, q); err != nil {
+			log.Errorf("Could not publish the message. err: %v", err)
+
+			time.Sleep(time.Second * 1)
+			continue
 		}
 	}
 }
