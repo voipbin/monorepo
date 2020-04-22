b57cb45594012696313bc05c7c424b580c1e496c
sungtae kim <pchero21@gmail.com>
2020-04-22 22:37:43 +0200

    Fixed scripts

--- scripts/database_scripts/table_calls.sql
+++ scripts/database_scripts/table_calls.sql
@@ -3,6 +3,8 @@ create table calls(
   id            binary(16),   -- id
   asterisk_id   varchar(255), -- Asterisk id
   channel_id    varchar(255), -- chcallsannel id
+  callflow_id   varchar(255), -- callflow id
+  service       varchar(16),  -- service
 
   -- source/destination
   source json, -- source's type, target, number, name, ...
@@ -15,21 +17,21 @@ create table calls(
   status            varchar(255), -- current status of call.
   data              json,         -- additional data. sip headers, and so on...
   direction         varchar(16),  -- direction of call. incoming/outgoing
-  service           varchar(16),  -- service
-  ended_by          varchar(16),  -- local/remote/empty for not sure.
+  hangup_by         varchar(16),  -- local/remote/empty for not sure.
+  hangup_reason     varchar(16),  -- reason
 
   -- timestamps
   tm_create datetime(6),  --
   tm_update datetime(6),  --
 
-  tm_answer datetime(6),  -- answer timestamp
-  tm_ringing datetime(6), -- rining timestamp
-  tm_end datetime(6),     -- end timestamp
+  tm_progressing  datetime(6), -- progrssing timestamp
+  tm_ringing      datetime(6), -- rining timestamp
+  tm_hangup       datetime(6), -- end timestamp
 
   primary key(id)
 );
 
 create index idx_calls_create on calls(tm_create);
-create index idx_calls_end on calls(tm_end);
+create index idx_calls_hangup on calls(tm_hangup);
 create index idx_calls_source_target on calls(source_target);
 create index idx_calls_destination_target on calls(destination_target);
