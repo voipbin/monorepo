5f2e7984d44b25dddaf916cfcfaa1c37226c1d65
sungtae kim <pchero21@gmail.com>
2020-06-03 05:22:27 +0200

    WIP

--- cmd/api-manager/main.go
+++ cmd/api-manager/main.go
@@ -1,7 +1,76 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"net/http"
+	"os"
+	"time"
+
+	"github.com/dgrijalva/jwt-go"
+	"github.com/gin-gonic/gin"
+	"github.com/sirupsen/logrus"
+)
+
+var (
+	router = gin.Default()
+)
+
+// User sample
+type User struct {
+	ID       uint64 `json:"id"`
+	Username string `json:"username"`
+	Password string `json:"password"`
+}
+
+var user = User{
+	ID:       1,
+	Username: "username",
+	Password: "password",
+}
 
 func main() {
 	fmt.Println("hello world")
+	router.Post("/login", Login)
+	logrus.Error(router.Run(":8080"))
+}
+
+// Login handles below request
+// /login
+func Login(c *gin.Context) {
+	var u User
+	if err := c.ShouldBindJSON(&u); err != nil {
+		c.JSON(http.StatusUnprocessableEntity, "Invalid json provided")
+		return
+	}
+
+	// compare the user from the request, with the one we defined:
+	if user.Username != u.Username || user.Password != u.Password {
+		c.JSON(http.StatusUnauthorized, "Please provide valid login details")
+		return
+	}
+
+	token, err := CreateToken(user.ID)
+	if err != nil {
+		c.JSON(http.StatusUnprocessableEntity, err.Error())
+		return
+	}
+
+	c.JSON(http.StatusOK, token)
+}
+
+// CreateToken creates jwt for user
+func CreateToken(userid uint64) (string, error) {
+	// creating access token
+	os.Setenv("ACCESS_SECRET", "skldjfsdlkflslje") // this should be in an env file
+	atClaims := jwt.MapClaims{}
+	atClaims["authorized"] = true
+	atClaims["user_id"] = userid
+	atClaims["exp"] = time.Now().Add(time.Minute * 15).Unix()
+	at := jwt.NewWithClaims(jwt.SigningMethodHS256, atClaims)
+	token, err := at.SignedString([]byte(os.Getenv("ACCESS_TOKEN")))
+	if err != nil {
+		return "", nil
+	}
+
+	return token, nil
 }
