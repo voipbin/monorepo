d9ae4c8f14f61827013abd62d9a8befd87aa118a
Sung-tae Kim <pchero21@gmail.com>
2020-09-04 12:36:27 +0000

    NOJIRA-Fix_parse_error

--- pkg/rabbitmq/main.go
+++ pkg/rabbitmq/main.go
@@ -16,21 +16,21 @@ type Request struct {
 	URI      string          `json:"uri"`
 	Method   RequestMethod   `json:"method"`
 	DataType string          `json:"data_type"`
-	Data     json.RawMessage `json:"data"`
+	Data     json.RawMessage `json:"data,omitempty"`
 }
 
 // Response struct
 type Response struct {
 	StatusCode int             `json:"status_code"`
 	DataType   string          `json:"data_type"`
-	Data       json.RawMessage `json:"data"`
+	Data       json.RawMessage `json:"data,omitempty"`
 }
 
 // Event struct
 type Event struct {
 	Type     EventType       `json:"type"`
 	DataType string          `json:"data_type"`
-	Data     json.RawMessage `json:"data"`
+	Data     json.RawMessage `json:"data,omitempty"`
 }
 
 // RequestMethod type
