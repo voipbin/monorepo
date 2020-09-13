3b79580f774079d6812d56fc7d7a0d9ec2ee8590
Sung-tae Kim <pchero21@gmail.com>
2020-09-13 01:34:02 +0000

    VOIP-227-Add_call_creation

--- pkg/listenhandler/request/main.go
+++ pkg/listenhandler/request/main.go
@@ -2,7 +2,6 @@ package request
 
 import (
 	"github.com/gofrs/uuid"
-
 	"gitlab.com/voipbin/bin-manager/flow-manager/pkg/flowhandler/models/action"
 )
 
@@ -11,12 +10,12 @@ import (
 // /v1/flows POST
 type V1DataFlowPost struct {
 	ID     uuid.UUID `json:"id"`      // flow's id
-	UserID int64     `json:"user_id"` // flow's owner
+	UserID uint64    `json:"user_id"` // flow's owner
 
 	Name   string `json:"name"`   // name
 	Detail string `json:"detail"` // detail
 
 	Actions []action.Action `json:"actions"` // actions
 
-	Psersist bool `json:"persist"` // persist. If it is true, set the flow into the database.
+	Persist bool `json:"persist"` // persist. If it is true, set the flow into the database.
 }
