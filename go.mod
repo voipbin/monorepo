d449e0bdcb960447526a8b22602cbb8a756ffcc6
Sungtae Kim <pchero21@gmail.com>
2021-04-20 12:14:15 +0900

    Fixed

--- go.mod
+++ go.mod
@@ -3,8 +3,8 @@ module gitlab.com/voipbin/bin-manager/call-manager.git
 go 1.14
 
 require (
-	github.com/go-redis/redis/v8 v8.7.1
-	github.com/go-sql-driver/mysql v1.5.0
+	github.com/go-redis/redis/v8 v8.8.2
+	github.com/go-sql-driver/mysql v1.6.0
 	github.com/gofrs/uuid v4.0.0+incompatible
 	github.com/golang/mock v1.5.0
 	github.com/joonix/log v0.0.0-20200409080653-9c1d2ceb5f1d
@@ -13,8 +13,11 @@ require (
 	github.com/sirupsen/logrus v1.8.1
 	github.com/smotes/purse v1.0.1
 	gitlab.com/voipbin/bin-manager/common-handler.git v0.0.0-20210314173554-61bfbbbd5633
-	gitlab.com/voipbin/bin-manager/flow-manager.git v0.0.0-20210322032029-24e9a68b7a4c
-	gitlab.com/voipbin/bin-manager/number-manager.git v0.0.0-20210321130210-a1303884d876
-	gitlab.com/voipbin/bin-manager/registrar-manager.git v0.0.0-20210321121353-4946f2db3798
-	gitlab.com/voipbin/bin-manager/tts-manager.git v0.0.0-20210317181219-9039fab1cff3
+	gitlab.com/voipbin/bin-manager/flow-manager.git v0.0.0-20210413034659-cbf35d7e5089
+	gitlab.com/voipbin/bin-manager/number-manager.git v0.0.0-20210405060100-f0029edb7aa2
+	gitlab.com/voipbin/bin-manager/registrar-manager.git v0.0.0-20210323024036-8c44f8dca2de
+	gitlab.com/voipbin/bin-manager/tts-manager.git v0.0.0-20210323024756-a7b0838b069d
+	golang.org/x/net v0.0.0-20210415231046-e915ea6b2b7d // indirect
+	golang.org/x/sys v0.0.0-20210419170143-37df388d1f33 // indirect
+	google.golang.org/genproto v0.0.0-20210416161957-9910b6c460de // indirect
 )
--- pkg/requesthandler/main.go
+++ pkg/requesthandler/main.go
@@ -11,15 +11,14 @@ import (
 
 	uuid "github.com/gofrs/uuid"
 	"github.com/prometheus/client_golang/prometheus"
-	log "github.com/sirupsen/logrus"
+	"github.com/sirupsen/logrus"
 
 	"gitlab.com/voipbin/bin-manager/call-manager.git/models/ari"
 	"gitlab.com/voipbin/bin-manager/call-manager.git/models/bridge"
 	"gitlab.com/voipbin/bin-manager/call-manager.git/models/channel"
+	"gitlab.com/voipbin/bin-manager/common-handler.git/pkg/rabbitmqhandler"
 	"gitlab.com/voipbin/bin-manager/flow-manager.git/models/action"
 	"gitlab.com/voipbin/bin-manager/flow-manager.git/models/activeflow"
-	// "gitlab.com/voipbin/bin-manager/call-manager.git/models/number"
-	"gitlab.com/voipbin/bin-manager/common-handler.git/pkg/rabbitmqhandler"
 	"gitlab.com/voipbin/bin-manager/number-manager.git/models/number"
 	"gitlab.com/voipbin/bin-manager/registrar-manager.git/models/astcontact"
 )
@@ -193,6 +192,72 @@ func uriUnescape(u string) string {
 	return res
 }
 
+// sendRequest sends a request to the given destination.
+func (r *requestHandler) sendRequest(queue string, uri string, method rabbitmqhandler.RequestMethod, resource resource, timeout, delayed int, dataType string, data json.RawMessage) (*rabbitmqhandler.Response, error) {
+
+	// creat a request message
+	req := &rabbitmqhandler.Request{
+		URI:      uri,
+		Method:   method,
+		DataType: dataType,
+		Data:     data,
+	}
+
+	log := logrus.WithFields(logrus.Fields{
+		"queue":   queue,
+		"delayed": delayed,
+		"request": req,
+	})
+	log.Debugf("Sending a request. queue: %s, method: %s, uri: %s", queue, method, uri)
+
+	ctx, cancel := context.WithTimeout(context.Background(), time.Second*time.Duration(timeout))
+	defer cancel()
+
+	switch {
+	case delayed > 0:
+		// send scheduled message.
+		// we don't expect the response message here.
+		if err := r.sendDelayedRequest(ctx, r.exchangeDelay, queue, resource, delayed, req); err != nil {
+			return nil, fmt.Errorf("could not publish the delayed request. err: %v", err)
+		}
+		return nil, nil
+
+	default:
+		res, err := r.sendDirectRequest(ctx, queue, resource, req)
+		if err != nil {
+			return nil, fmt.Errorf("could not publish the RPC. err: %v", err)
+		}
+
+		log.WithFields(logrus.Fields{
+			"response": res,
+		}).Debugf("Received result. queue: %s, method: %s, uri: %s, status_code: %d", queue, method, uri, res.StatusCode)
+		return res, nil
+	}
+}
+
+// sendDirectRequest sends the request to the target without delay
+func (r *requestHandler) sendDirectRequest(ctx context.Context, target string, resource resource, req *rabbitmqhandler.Request) (*rabbitmqhandler.Response, error) {
+
+	start := time.Now()
+	res, err := r.sock.PublishRPC(ctx, target, req)
+	elapsed := time.Since(start)
+	promRequestProcessTime.WithLabelValues(target, string(resource), string(req.Method)).Observe(float64(elapsed.Milliseconds()))
+
+	return res, err
+}
+
+// sendDelayedRequest sends the delayed request to the target
+// delay unit is millisecond.
+func (r *requestHandler) sendDelayedRequest(ctx context.Context, target string, queue string, resource resource, delay int, req *rabbitmqhandler.Request) error {
+
+	start := time.Now()
+	err := r.sock.PublishExchangeDelayedRequest(r.exchangeDelay, queue, req, delay)
+	elapsed := time.Since(start)
+	promRequestProcessTime.WithLabelValues(target, string(resource), string(req.Method)).Observe(float64(elapsed.Milliseconds()))
+
+	return err
+}
+
 // SendARIRequest send a request to the Asterisk-proxy and return the response
 func (r *requestHandler) sendRequestAst(asteriskID, uri string, method rabbitmqhandler.RequestMethod, resource resource, timeout int, delayed int, dataType string, data []byte) (*rabbitmqhandler.Response, error) {
 
@@ -233,73 +298,4 @@ func (r *requestHandler) sendRequestNumber(uri string, method rabbitmqhandler.Re
 
 	return r.sendRequest(r.queueNumber, uri, method, resource, timeout, delayed, dataType, data)
 
-}
-
-// sendRequest sends a request to the given destination.
-func (r *requestHandler) sendRequest(queue string, uri string, method rabbitmqhandler.RequestMethod, resource resource, timeout, delayed int, dataType string, data json.RawMessage) (*rabbitmqhandler.Response, error) {
-
-	log.WithFields(log.Fields{
-		"queue":     queue,
-		"method":    method,
-		"uri":       uri,
-		"data_type": dataType,
-		"delayed":   delayed,
-	}).Debugf("Sending a request. queue: %s, method: %s, uri: %s", queue, method, uri)
-
-	// creat a request message
-	req := &rabbitmqhandler.Request{
-		URI:      uri,
-		Method:   method,
-		DataType: dataType,
-		Data:     data,
-	}
-
-	ctx, cancel := context.WithTimeout(context.Background(), time.Second*time.Duration(timeout))
-	defer cancel()
-
-	switch {
-	case delayed > 0:
-		// send scheduled message.
-		// we don't expect the response message here.
-		if err := r.sendDelayedRequest(ctx, r.exchangeDelay, queue, resource, delayed, req); err != nil {
-			return nil, fmt.Errorf("could not publish the delayed request. err: %v", err)
-		}
-		return nil, nil
-
-	default:
-		res, err := r.sendDirectRequest(ctx, queue, resource, req)
-		if err != nil {
-			return nil, fmt.Errorf("could not publish the RPC. err: %v", err)
-		}
-
-		log.WithFields(log.Fields{
-			"method": method,
-			"uri":    uri,
-			"res":    res,
-		}).Debugf("Received result. queue: %s, method: %s, uri: %s, status_code: %d", queue, method, uri, res.StatusCode)
-		return res, nil
-	}
-}
-
-// sendDirectRequest sends the request to the target without delay
-func (r *requestHandler) sendDirectRequest(ctx context.Context, target string, resource resource, req *rabbitmqhandler.Request) (*rabbitmqhandler.Response, error) {
-
-	start := time.Now()
-	res, err := r.sock.PublishRPC(ctx, target, req)
-	elapsed := time.Since(start)
-	promRequestProcessTime.WithLabelValues(target, string(resource), string(req.Method)).Observe(float64(elapsed.Milliseconds()))
-
-	return res, err
-}
-
-// sendDelayedRequest sends the delayed request to the target
-// delay unit is millisecond.
-func (r *requestHandler) sendDelayedRequest(ctx context.Context, target string, queue string, resource resource, delay int, req *rabbitmqhandler.Request) error {
-
-	start := time.Now()
-	err := r.sock.PublishExchangeDelayedRequest(r.exchangeDelay, queue, req, delay)
-	elapsed := time.Since(start)
-	promRequestProcessTime.WithLabelValues(target, string(resource), string(req.Method)).Observe(float64(elapsed.Milliseconds()))
-
-	return err
 }
