53f251aa072b4ff6718f06945a433d578d5f9258
Sungtae Kim <pchero21@gmail.com>
2021-09-09 05:29:05 +0000

    NOJIRA-Fix_webhook_data_type

--- pkg/listenhandler/v1_webhooks.go
+++ pkg/listenhandler/v1_webhooks.go
@@ -7,6 +7,7 @@ import (
 	"github.com/sirupsen/logrus"
 
 	"gitlab.com/voipbin/bin-manager/common-handler.git/pkg/rabbitmqhandler"
+	"gitlab.com/voipbin/bin-manager/webhook-manager.git/models/webhook"
 	"gitlab.com/voipbin/bin-manager/webhook-manager.git/pkg/listenhandler/models/request"
 )
 
@@ -23,7 +24,20 @@ func (h *listenHandler) processV1WebhooksPost(m *rabbitmqhandler.Request) (*rabb
 		return simpleResponse(400), nil
 	}
 
-	if err := h.whHandler.SendWebhook(&reqData.Webhook); err != nil {
+	d, err := json.Marshal(reqData.Data)
+	if err != nil {
+		logrus.Errorf("Could not marshal the message. message: %v, err: %v", reqData.Data, err)
+		return simpleResponse(400), nil
+	}
+
+	wh := &webhook.Webhook{
+		Method:     reqData.Method,
+		WebhookURI: reqData.WebhookURI,
+		DataType:   reqData.DataType,
+		Data:       d,
+	}
+
+	if err := h.whHandler.SendWebhook(wh); err != nil {
 		logrus.Debugf("Could not send the webhook correctly. err: %v", err)
 		return simpleResponse(500), nil
 	}
