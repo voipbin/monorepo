f3f477f668f92aaed5dbe5057186f96c8d1402f8
Sungtae Kim <pchero21@gmail.com>
2022-01-25 17:04:31 +0000

    NOJIRA-Fix_login_post

--- pkg/listenhandler/models/request/login.go
+++ pkg/listenhandler/models/request/login.go
@@ -1,9 +1,9 @@
 package request
 
-// V1DataLogin is
+// V1DataLoginPost is
 // v1 data type request struct for
 // /v1/login POST
-type V1DataLogin struct {
+type V1DataLoginPost struct {
 	Username string `json:"username"`
 	Password string `json:"password"`
 }
