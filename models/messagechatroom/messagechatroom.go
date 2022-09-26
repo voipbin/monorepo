46026009944f064924c99701cb4fed5ec9e5e25d
Sungtae Kim <pchero21@gmail.com>
2022-09-26 02:36:00 +0000

    NOJIRA-Fix_messagechatroom_struct

--- models/messagechatroom/messagechatroom.go
+++ models/messagechatroom/messagechatroom.go
@@ -17,10 +17,10 @@ type Messagechatroom struct {
 	MessagechatID uuid.UUID `json:"messagechat_id"`
 
 	// message defines
-	Source *commonaddress.Address
-	Type   Type
-	Text   string
-	Medias []media.Media
+	Source *commonaddress.Address `json:"source"`
+	Type   Type                   `json:"type"`
+	Text   string                 `json:"text"`
+	Medias []media.Media          `json:"medias"`
 
 	TMCreate string `json:"tm_create"`
 	TMUpdate string `json:"tm_update"`
