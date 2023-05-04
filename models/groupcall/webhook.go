78332eb39fad30621ab19e359847277b60ee12e6
Sungtae Kim <pchero21@gmail.com>
2023-05-04 17:47:21 +0000

    NOJIRA-Add_groupcall_webhook

--- models/groupcall/webhook.go
+++ models/groupcall/webhook.go
@@ -18,11 +18,21 @@ type WebhookMessage struct {
 	Source       *commonaddress.Address  `json:"source"`
 	Destinations []commonaddress.Address `json:"destinations"`
 
+	MasterCallID      uuid.UUID `json:"master_call_id,omitempty"`
+	MasterGroupcallID uuid.UUID `json:"master_groupcall_id,omitempty"`
+
 	RingMethod   RingMethod   `json:"ring_method"`
 	AnswerMethod AnswerMethod `json:"answer_method"`
 
-	AnswerCallID uuid.UUID   `json:"answer_call_id"` // valid only when the answered_method  is hangup others
+	AnswerCallID uuid.UUID   `json:"answer_call_id"` // represents answered call id.
 	CallIDs      []uuid.UUID `json:"call_ids"`
+
+	AnswerGroupcallID uuid.UUID   `json:"answer_groupcall_id"` // represents answered groupcall id
+	GroupcallIDs      []uuid.UUID `json:"groupcall_ids"`
+
+	CallCount      int `json:"call_count"`           // represent left number of calls for current dial
+	GroupcallCount int `json:"groupcall_count"`      // represent left number of groupcalls for current dial
+	DialIndex      int `json:"dial_index,omitempty"` // represent current dial index. valid only ringmethod is ringall
 
 	// timestamp
 	TMCreate string `json:"tm_create"`
@@ -42,11 +52,21 @@ func (h *Groupcall) ConvertWebhookMessage() *WebhookMessage {
 		Source:       h.Source,
 		Destinations: h.Destinations,
 
+		MasterCallID:      h.MasterCallID,
+		MasterGroupcallID: h.MasterGroupcallID,
+
 		RingMethod:   h.RingMethod,
 		AnswerMethod: h.AnswerMethod,
 
 		AnswerCallID: h.AnswerCallID,
 		CallIDs:      h.CallIDs,
+
+		AnswerGroupcallID: h.AnswerCallID,
+		GroupcallIDs:      h.GroupcallIDs,
+
+		CallCount:      h.CallCount,
+		GroupcallCount: h.GroupcallCount,
+		DialIndex:      h.DialIndex,
 
 		TMCreate: h.TMCreate,
 		TMUpdate: h.TMUpdate,
