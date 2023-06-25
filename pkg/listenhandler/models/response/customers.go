94faf5a2cc27fe71f3165a63732845d3b77703f1
Sungtae Kim <pchero21@gmail.com>
2023-06-25 14:50:22 +0000

    NOJIRA-Add_account_control

--- pkg/listenhandler/models/response/customers.go
+++ pkg/listenhandler/models/response/customers.go
@@ -1,8 +1,8 @@
 package response
 
-// V1ResponseCustomerIDIsValidBalance is
+// V1ResponseCustomersIDIsValidBalancePost is
 // v1 response type for
 // /v1/customers/<customer-id>/is_valid_balance POST
-type V1ResponseCustomerIDIsValidBalance struct {
+type V1ResponseCustomersIDIsValidBalancePost struct {
 	Valid bool `json:"valid"`
 }
