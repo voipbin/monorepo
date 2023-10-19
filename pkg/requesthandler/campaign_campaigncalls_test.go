388205d7bd9cc747f35ec04151de1f148b690545
Sungtae Kim <pchero21@gmail.com>
2023-10-19 02:25:48 +0000

    VOIP-790-Add_campaigncalls

--- pkg/requesthandler/campaign_campaigncalls_test.go
+++ pkg/requesthandler/campaign_campaigncalls_test.go
@@ -14,6 +14,71 @@ import (
 	"gitlab.com/voipbin/bin-manager/common-handler.git/pkg/rabbitmqhandler"
 )
 
+func Test_CampaignV1CampaigncallGets(t *testing.T) {
+
+	tests := []struct {
+		name string
+
+		pageToken string
+		pageSize  uint64
+
+		response *rabbitmqhandler.Response
+
+		expectTarget  string
+		expectRequest *rabbitmqhandler.Request
+		expectResult  []cacampaigncall.Campaigncall
+	}{
+		{
+			"normal",
+
+			"2020-09-20 03:23:20.995000",
+			10,
+
+			&rabbitmqhandler.Response{
+				StatusCode: 200,
+				DataType:   "application/json",
+				Data:       []byte(`[{"id":"0dccc282-6e23-11ee-8173-23149728867a"}]`),
+			},
+
+			"bin-manager.campaign-manager.request",
+			&rabbitmqhandler.Request{
+				URI:      fmt.Sprintf("/v1/campaigncalls?page_token=%s&page_size=10", url.QueryEscape("2020-09-20 03:23:20.995000")),
+				Method:   rabbitmqhandler.RequestMethodGet,
+				DataType: ContentTypeJSON,
+			},
+			[]cacampaigncall.Campaigncall{
+				{
+					ID: uuid.FromStringOrNil("0dccc282-6e23-11ee-8173-23149728867a"),
+				},
+			},
+		},
+	}
+
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			mc := gomock.NewController(t)
+			defer mc.Finish()
+
+			mockSock := rabbitmqhandler.NewMockRabbit(mc)
+			reqHandler := requestHandler{
+				sock: mockSock,
+			}
+
+			ctx := context.Background()
+			mockSock.EXPECT().PublishRPC(gomock.Any(), tt.expectTarget, tt.expectRequest).Return(tt.response, nil)
+
+			res, err := reqHandler.CampaignV1CampaigncallGets(ctx, tt.pageToken, tt.pageSize)
+			if err != nil {
+				t.Errorf("Wrong match. expect: ok, got: %v", err)
+			}
+
+			if reflect.DeepEqual(tt.expectResult, res) == false {
+				t.Errorf("Wrong match.\nexpect: %v\ngot: %v\n", tt.expectResult, res)
+			}
+		})
+	}
+}
+
 func Test_CampaignV1CampaigncallGetsByCampaignID(t *testing.T) {
 
 	tests := []struct {
