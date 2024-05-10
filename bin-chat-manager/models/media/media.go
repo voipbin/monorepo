dc57f574600dc1f70c24dbb48a301e2177220a68
Sungtae Kim <pchero21@gmail.com>
2024-05-11 01:29:37 +0900

    WIP

--- bin-chat-manager/models/media/media.go
+++ bin-chat-manager/models/media/media.go
@@ -1,5 +1,25 @@
 package media
 
+import (
+	commonaddress "monorepo/bin-common-handler/models/address"
+
+	"github.com/gofrs/uuid"
+)
+
 // Media define
 type Media struct {
+	Type    Type                  `json:"type,omitempty"`
+	Address commonaddress.Address `json:"address,omitempty"` // valid only if the Type is address type
+	FileID  uuid.UUID             `json:"file_id,omitempty"` // valid only if the Type is file
+	Link    string                `json:"link,omitempty"`    // valid only if the Type is link type
 }
+
+// Type define
+type Type string
+
+// list of types
+const (
+	TypeAddress Type = "address" // the media contains address info
+	TypeFile    Type = "file"    // the media contains file info
+	TypeLink    Type = "link"    // the media contains link info
+)
