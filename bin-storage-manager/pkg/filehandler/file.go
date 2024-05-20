65a2c5676408cb4cff67a8434157cdfd683f5bf9
Sungtae Kim <pchero21@gmail.com>
2024-05-20 17:26:48 +0900

    WIP

--- bin-storage-manager/pkg/filehandler/file.go
+++ bin-storage-manager/pkg/filehandler/file.go
@@ -60,7 +60,7 @@ func (h *fileHandler) Create(
 	expireDuration := 3650 * 24 * time.Hour // valid for 10 years
 	tmExpire := time.Now().UTC().Add(expireDuration)
 	tmDownloadExpire := h.utilHandler.TimeGetCurTimeAdd(expireDuration)
-	downloadURI, err := h.bucketfileGenerateDownloadURI(h.bucketMedia, filepath, tmExpire)
+	downloadURI, err := h.bucketfileGenerateDownloadURI(h.bucketMedia, dstFilepath, tmExpire)
 	if err != nil {
 		log.Errorf("Could not generate download URI. err: %v", err)
 		return nil, err
