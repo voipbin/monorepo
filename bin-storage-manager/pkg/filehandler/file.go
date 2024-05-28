37a74d82ccfe391359173e56f9bed50ab6f8ac02
Sungtae Kim <pchero21@gmail.com>
2024-05-28 16:21:35 +0900

    WIP

--- bin-storage-manager/pkg/filehandler/file.go
+++ bin-storage-manager/pkg/filehandler/file.go
@@ -127,6 +127,116 @@ func (h *fileHandler) Create(
 	return res, nil
 }
 
+// Create Creates the file and returns the created file
+func (h *fileHandler) CreateOrg(
+	ctx context.Context,
+	customerID uuid.UUID,
+	ownerID uuid.UUID,
+	referenceType file.ReferenceType,
+	referenceID uuid.UUID,
+	name string,
+	detail string,
+	filename string,
+	bucketName string,
+	filepath string,
+) (*file.File, error) {
+	log := logrus.WithFields(logrus.Fields{
+		"func":           "Create",
+		"customer_id":    customerID,
+		"owner_id":       ownerID,
+		"reference_type": referenceType,
+		"reference_id":   referenceID,
+		"name":           name,
+		"detail":         detail,
+		"filename":       filename,
+		"bucket_name":    bucketName,
+		"filepath":       filepath,
+	})
+
+	// check file does exist
+	// we are expecting all original files are located in the tmp bucket
+	attrs, err := h.bucketfileGetAttrs(ctx, bucketName, filepath)
+	if err != nil {
+		log.Errorf("Could not get attrs. Considering the file does not exist. err: %v", err)
+		return nil, errors.Wrapf(err, "file does not exist")
+	}
+	log.WithField("attrs", attrs).Debugf("Found file. name: %s", attrs.Name)
+
+	// validate the account
+	a, err := h.accountHandler.ValidateFileInfoByCustomerID(ctx, customerID, 1, attrs.Size)
+	if err != nil {
+		log.Errorf("Could not pass the account validation. err: %v", err)
+		return nil, errors.Wrapf(err, "could not pass the account validation")
+	}
+	log.WithField("account", a).Debugf("Validated the account. account_id: %s", a.ID)
+
+	// generate destination filepath
+	id := h.utilHandler.UUIDCreate()
+	dstFilepath := fmt.Sprintf("%s/%s", bucketDirectoryBin, id)
+
+	// move the file from the tmp bucket to the new location
+	dstAttrs, err := h.bucketfileMove(ctx, bucketName, filepath, h.bucketMedia, dstFilepath)
+	if err != nil {
+		log.Errorf("Could not move the file. err: %v", err)
+		return nil, err
+	}
+	log.WithField("dst_attrs", dstAttrs).Debugf("Moved file. bucket_link: %s", dstAttrs.MediaLink)
+
+	// get dowload uri
+	expireDuration := 3650 * 24 * time.Hour // valid for 10 years
+	tmExpire := time.Now().UTC().Add(expireDuration)
+	tmDownloadExpire := h.utilHandler.TimeGetCurTimeAdd(expireDuration)
+
+	downloadURI, err := h.bucketfileGenerateDownloadURI(h.bucketMedia, dstFilepath, tmExpire)
+	if err != nil {
+		log.Errorf("Could not generate download URI. err: %v", err)
+		return nil, err
+	}
+
+	// create db row
+	f := &file.File{
+		ID:               id,
+		CustomerID:       customerID,
+		AccountID:        a.ID,
+		OwnerID:          ownerID,
+		ReferenceType:    referenceType,
+		ReferenceID:      referenceID,
+		Name:             name,
+		Detail:           detail,
+		BucketName:       h.bucketMedia,
+		Filename:         filename,
+		Filepath:         dstFilepath,
+		Filesize:         attrs.Size,
+		URIBucket:        dstAttrs.MediaLink,
+		URIDownload:      downloadURI,
+		TMDownloadExpire: tmDownloadExpire,
+	}
+
+	if errCreate := h.db.FileCreate(ctx, f); errCreate != nil {
+		log.Errorf("Could not create file. err: %v", errCreate)
+		return nil, errCreate
+	}
+
+	// get created file
+	res, err := h.db.FileGet(ctx, id)
+	if err != nil {
+		log.Errorf("Could not get created file info. err: %v", err)
+		return nil, err
+	}
+	log.WithField("file", res).Debugf("Created file info. id: %s", res.ID)
+
+	h.notifyHandler.PublishEvent(ctx, file.EventTypeFileCreated, res)
+
+	// increase account's file info
+	_, err = h.accountHandler.IncreaseFileInfo(ctx, res.AccountID, 1, res.Filesize)
+	if err != nil {
+		log.Errorf("Could not increase account's file info. err: %v", err)
+		// we got error here, but just write the error message only.
+	}
+
+	return res, nil
+}
+
 // Get returns the file info
 func (h *fileHandler) Get(ctx context.Context, id uuid.UUID) (*file.File, error) {
 	res, err := h.db.FileGet(ctx, id)
