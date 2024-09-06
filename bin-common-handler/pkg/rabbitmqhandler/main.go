4c3af5f038235c69dfd5546745460b7ea2d29273
Sungtae Kim <pchero21@gmail.com>
2024-09-07 01:39:48 +0900

    WIP

--- bin-common-handler/pkg/rabbitmqhandler/main.go
+++ bin-common-handler/pkg/rabbitmqhandler/main.go
@@ -11,41 +11,6 @@ import (
 	"github.com/sirupsen/logrus"
 )
 
-// // Request struct
-// type Request struct {
-// 	URI       string          `json:"uri"`
-// 	Method    RequestMethod   `json:"method"`
-// 	Publisher string          `json:"publisher"`
-// 	DataType  string          `json:"data_type"`
-// 	Data      json.RawMessage `json:"data,omitempty"`
-// }
-
-// // Response struct
-// type Response struct {
-// 	StatusCode int             `json:"status_code"`
-// 	DataType   string          `json:"data_type"`
-// 	Data       json.RawMessage `json:"data,omitempty"`
-// }
-
-// // Event struct
-// type Event struct {
-// 	Type      string          `json:"type"`
-// 	Publisher string          `json:"publisher"`
-// 	DataType  string          `json:"data_type"`
-// 	Data      json.RawMessage `json:"data,omitempty"`
-// }
-
-// RequestMethod type
-type RequestMethod string
-
-// List of RequestMethod
-const (
-	RequestMethodPost   RequestMethod = "POST"
-	RequestMethodGet    RequestMethod = "GET"
-	RequestMethodPut    RequestMethod = "PUT"
-	RequestMethodDelete RequestMethod = "DELETE"
-)
-
 // Rabbit defines rabbit queue interfaces
 type Rabbit interface {
 	Connect()
