197d4499081f4f142b5cfccb5b18eaaabcd3ba8c
Sungtae Kim <pchero21@gmail.com>
2024-11-12 18:44:54 +0900

    VOIP-944-Change_get_env_variablemain_2 (#67)
    * bin-billing-manager
    * bin-call-manager
    * bin-campaign-manager
    * bin-chat-manager
    * bin-conference-manager
    * bin-conversation-manager
    * bin-customer-manager
    * bin-flow-manager
    * bin-hook-manager
    * bin-message-manager
    * bin-number-manager

--- bin-hook-manager/cmd/hook-manager/main.go
+++ bin-hook-manager/cmd/hook-manager/main.go
@@ -3,9 +3,13 @@ package main
 import (
 	"database/sql"
 	"encoding/base64"
-	"flag"
+	"net/http"
 	"os"
 	"time"
+
+	"github.com/prometheus/client_golang/prometheus/promhttp"
+	"github.com/spf13/pflag"
+	"github.com/spf13/viper"
 
 	commonoutline "monorepo/bin-common-handler/models/outline"
 	"monorepo/bin-common-handler/models/sock"
@@ -26,12 +30,23 @@ import (
 
 const serviceName = commonoutline.ServiceNameHookManager
 
-var dsn = flag.String("dsn", "testid:testpassword@tcp(127.0.0.1:3306)/test", "database dsn")
+const (
+	defaultDatabaseDSN             = "testid:testpassword@tcp(127.0.0.1:3306)/test"
+	defaultPrometheusEndpoint      = "/metrics"
+	defaultPrometheusListenAddress = ":2112"
+	defaultRabbitMQAddress         = "amqp://guest:guest@localhost:5672"
+	defaultSSLPrivkeyBase64        = ""
+	defaultSSLCertBase64           = ""
+)
 
-var sslPrivkeyBase64 = flag.String("ssl_private_base64", "", "Base64 encoded private key for ssl connection.")
-var sslCertBase64 = flag.String("ssl_cert_base64", "", "Base64 encoded cert key for ssl connection.")
-
-var rabbitAddr = flag.String("rabbit_addr", "amqp://guest:guest@localhost:5672", "rabbitmq service address.")
+var (
+	databaseDSN             = ""
+	prometheusEndpoint      = ""
+	prometheusListenAddress = ""
+	rabbitMQAddress         = ""
+	sslPrivkeyBase64        = ""
+	sslCertBase64           = ""
+)
 
 const (
 	constSSLPrivFilename = "/tmp/ssl_privkey.pem"
@@ -53,7 +68,7 @@ func main() {
 	log := logrus.WithField("func", "main")
 
 	// connect to database
-	sqlDB, err := sql.Open("mysql", *dsn)
+	sqlDB, err := sql.Open("mysql", databaseDSN)
 	if err != nil {
 		log.Errorf("Could not access to database. err: %v", err)
 		return
@@ -61,7 +76,7 @@ func main() {
 	defer sqlDB.Close()
 
 	// connect to rabbitmq
-	sock := sockhandler.NewSockHandler(sock.TypeRabbitMQ, *rabbitAddr)
+	sock := sockhandler.NewSockHandler(sock.TypeRabbitMQ, rabbitMQAddress)
 	sock.Connect()
 
 	// create servicehandler
@@ -106,23 +121,126 @@ func main() {
 }
 
 func init() {
-	flag.Parse()
+	initVariable()
 
 	// init log
 	logrus.SetFormatter(joonix.NewFormatter())
 	logrus.SetLevel(logrus.DebugLevel)
 
+	initProm(prometheusEndpoint, prometheusListenAddress)
+
 	// init ssl
-	if errWrite := writeBase64(constSSLCertFilename, *sslCertBase64); errWrite != nil {
+	if errWrite := writeBase64(constSSLCertFilename, sslCertBase64); errWrite != nil {
 		logrus.Errorf("Could not write the ssl cert file.")
 		return
 	}
 
-	if errWrite := writeBase64(constSSLPrivFilename, *sslPrivkeyBase64); errWrite != nil {
+	if errWrite := writeBase64(constSSLPrivFilename, sslPrivkeyBase64); errWrite != nil {
 		logrus.Errorf("Could not write the ssl private key file.")
 		return
 	}
 
+}
+
+func initVariable() {
+	log := logrus.WithField("func", "initVariable")
+	viper.AutomaticEnv()
+
+	pflag.String("rabbitmq_address", defaultRabbitMQAddress, "Address of the RabbitMQ server (e.g., amqp://guest:guest@localhost:5672)")
+	pflag.String("prometheus_endpoint", defaultPrometheusEndpoint, "URL for the Prometheus metrics endpoint")
+	pflag.String("prometheus_listen_address", defaultPrometheusListenAddress, "Address for Prometheus to listen on (e.g., localhost:8080)")
+	pflag.String("database_dsn", defaultDatabaseDSN, "Data Source Name for database connection (e.g., user:password@tcp(localhost:3306)/dbname)")
+	pflag.String("ssl_privkey_base64", defaultSSLPrivkeyBase64, "Base64-encoded private key")
+	pflag.String("ssl_cert_base64", defaultSSLCertBase64, "Base64-encoded cert")
+
+	pflag.Parse()
+
+	// rabbitmq_address
+	if errFlag := viper.BindPFlag("rabbitmq_address", pflag.Lookup("rabbitmq_address")); errFlag != nil {
+		log.Errorf("Error binding flag: %v", errFlag)
+		panic(errFlag)
+	}
+	if errEnv := viper.BindEnv("rabbitmq_address", "RABBITMQ_ADDRESS"); errEnv != nil {
+		log.Errorf("Error binding env: %v", errEnv)
+		panic(errEnv)
+	}
+	rabbitMQAddress = viper.GetString("rabbitmq_address")
+
+	// prometheus_endpoint
+	if errFlag := viper.BindPFlag("prometheus_endpoint", pflag.Lookup("prometheus_endpoint")); errFlag != nil {
+		log.Errorf("Error binding flag: %v", errFlag)
+		panic(errFlag)
+	}
+	if errEnv := viper.BindEnv("prometheus_endpoint", "PROMETHEUS_ENDPOINT"); errEnv != nil {
+		log.Errorf("Error binding env: %v", errEnv)
+		panic(errEnv)
+	}
+	prometheusEndpoint = viper.GetString("prometheus_endpoint")
+
+	// prometheus_listen_address
+	if errFlag := viper.BindPFlag("prometheus_listen_address", pflag.Lookup("prometheus_listen_address")); errFlag != nil {
+		log.Errorf("Error binding flag: %v", errFlag)
+		panic(errFlag)
+	}
+	if errEnv := viper.BindEnv("prometheus_listen_address", "PROMETHEUS_LISTEN_ADDRESS"); errEnv != nil {
+		log.Errorf("Error binding env: %v", errEnv)
+		panic(errEnv)
+	}
+	prometheusListenAddress = viper.GetString("prometheus_listen_address")
+
+	// database_dsn
+	if errFlag := viper.BindPFlag("database_dsn", pflag.Lookup("database_dsn")); errFlag != nil {
+		log.Errorf("Error binding flag: %v", errFlag)
+		panic(errFlag)
+	}
+	if errEnv := viper.BindEnv("database_dsn", "DATABASE_DSN"); errEnv != nil {
+		log.Errorf("Error binding env: %v", errEnv)
+		panic(errEnv)
+	}
+	databaseDSN = viper.GetString("database_dsn")
+
+	// ssl_privkey_base64
+	if errFlag := viper.BindPFlag("ssl_privkey_base64", pflag.Lookup("ssl_privkey_base64")); errFlag != nil {
+		log.Errorf("Error binding flag: %v", errFlag)
+		panic(errFlag)
+	}
+	if errEnv := viper.BindEnv("ssl_privkey_base64", "SSL_PRIVKEY_BASE64"); errEnv != nil {
+		log.Errorf("Error binding env: %v", errEnv)
+		panic(errEnv)
+	}
+	sslPrivkeyBase64 = viper.GetString("ssl_privkey_base64")
+
+	// ssl_cert_base64
+	if errFlag := viper.BindPFlag("ssl_cert_base64", pflag.Lookup("ssl_cert_base64")); errFlag != nil {
+		log.Errorf("Error binding flag: %v", errFlag)
+		panic(errFlag)
+	}
+	if errEnv := viper.BindEnv("ssl_cert_base64", "SSL_CERT_BASE64"); errEnv != nil {
+		log.Errorf("Error binding env: %v", errEnv)
+		panic(errEnv)
+	}
+	sslCertBase64 = viper.GetString("ssl_cert_base64")
+}
+
+// initProm inits prometheus settings
+func initProm(endpoint, listen string) {
+	log := logrus.WithField("func", "initProm").WithFields(logrus.Fields{
+		"endpoint": endpoint,
+		"listen":   listen,
+	})
+
+	http.Handle(endpoint, promhttp.Handler())
+	go func() {
+		for {
+			if errListen := http.ListenAndServe(listen, nil); errListen != nil {
+				log.Errorf("Could not start prometheus listener. err: %v", errListen)
+				time.Sleep(time.Second * 1)
+				continue
+			}
+			log.Infof("Finishing the prometheus listener.")
+			break
+		}
+	}()
 }
 
 func writeBase64(filename string, data string) error {
--- bin-number-manager/cmd/number-manager/main.go
+++ bin-number-manager/cmd/number-manager/main.go
@@ -2,12 +2,14 @@ package main
 
 import (
 	"database/sql"
-	"flag"
 	"net/http"
 	"os"
 	"os/signal"
 	"syscall"
 	"time"
+
+	"github.com/spf13/pflag"
+	"github.com/spf13/viper"
 
 	commonoutline "monorepo/bin-common-handler/models/outline"
 	"monorepo/bin-common-handler/models/sock"
@@ -35,36 +37,44 @@ const serviceName = commonoutline.ServiceNameNumberManager
 var chSigs = make(chan os.Signal, 1)
 var chDone = make(chan bool, 1)
 
-// args for rabbitmq
-var argRabbitAddr = flag.String("rabbit_addr", "amqp://guest:guest@localhost:5672", "rabbitmq service address.")
+const (
+	defaultDatabaseDSN             = "testid:testpassword@tcp(127.0.0.1:3306)/test"
+	defaultPrometheusEndpoint      = "/metrics"
+	defaultPrometheusListenAddress = ":2112"
+	defaultRabbitMQAddress         = "amqp://guest:guest@localhost:5672"
+	defaultRedisAddress            = "127.0.0.1:6379"
+	defaultRedisDatabase           = 1
+	defaultRedisPassword           = ""
 
-// args for prometheus
-var argPromEndpoint = flag.String("prom_endpoint", "/metrics", "endpoint for prometheus metric collecting.")
-var argPromListenAddr = flag.String("prom_listen_addr", ":2112", "endpoint for prometheus metric collecting.")
+	defaultTwilioSID          = ""
+	defaultTwilioToken        = ""
+	defaultTelnyxConnectionID = ""
+	defaultTelnyxProfileID    = ""
+	defaultTelnyxToken        = ""
+)
 
-// args for database
-var argDBDSN = flag.String("dbDSN", "testid:testpassword@tcp(127.0.0.1:3306)/test", "database dsn for number-manager.")
+var (
+	databaseDSN             = ""
+	prometheusEndpoint      = ""
+	prometheusListenAddress = ""
+	rabbitMQAddress         = ""
+	redisAddress            = ""
+	redisDatabase           = 0
+	redisPassword           = ""
 
-// args for redis
-var argRedisAddr = flag.String("redis_addr", "127.0.0.1:6379", "redis address.")
-var argRedisPassword = flag.String("redis_password", "", "redis password")
-var argRedisDB = flag.Int("redis_db", 1, "redis database.")
-
-// args for twilio
-var argTwilioSID = flag.String("twilio_sid", "", "twilio's sid.")
-var argTwilioToken = flag.String("twilio_token", "", "twilio's token.")
-
-// args for telnyx
-var argTelnyxConnectionID = flag.String("telnyx_connection_id", "", "telnyx's connection id.")
-var argTelnyxProfileID = flag.String("telnyx_profile_id", "", "telnyx's profile id.")
-var argTelnyxToken = flag.String("telnyx_token", "", "telnyx's token.")
+	twilioSID          = ""
+	twilioToken        = ""
+	telnyxConnectionID = ""
+	telnyxProfileID    = ""
+	telnyxToken        = ""
+)
 
 func main() {
 	log := logrus.WithField("func", "main")
 	log.Debugf("Hello world. Starting number-manager.")
 
 	// connect to database
-	sqlDB, err := sql.Open("mysql", *argDBDSN)
+	sqlDB, err := sql.Open("mysql", databaseDSN)
 	if err != nil {
 		log.Errorf("Could not access to database. err: %v", err)
 		return
@@ -72,7 +82,7 @@ func main() {
 	defer sqlDB.Close()
 
 	// connect to cache
-	cache := cachehandler.NewHandler(*argRedisAddr, *argRedisPassword, *argRedisDB)
+	cache := cachehandler.NewHandler(redisAddress, redisPassword, redisDatabase)
 	if err := cache.Connect(); err != nil {
 		log.Errorf("Could not connect to cache server. err: %v", err)
 		return
@@ -86,7 +96,7 @@ func main() {
 
 // proces init
 func init() {
-	flag.Parse()
+	initVariable()
 
 	// init logs
 	initLog()
@@ -95,9 +105,162 @@ func init() {
 	initSignal()
 
 	// init prometheus setting
-	initProm(*argPromEndpoint, *argPromListenAddr)
+	initProm(prometheusEndpoint, prometheusListenAddress)
 
 	logrus.Info("init finished.")
+}
+
+func initVariable() {
+	log := logrus.WithField("func", "initVariable")
+	viper.AutomaticEnv()
+
+	pflag.String("rabbitmq_address", defaultRabbitMQAddress, "Address of the RabbitMQ server (e.g., amqp://guest:guest@localhost:5672)")
+	pflag.String("prometheus_endpoint", defaultPrometheusEndpoint, "URL for the Prometheus metrics endpoint")
+	pflag.String("prometheus_listen_address", defaultPrometheusListenAddress, "Address for Prometheus to listen on (e.g., localhost:8080)")
+	pflag.String("database_dsn", defaultDatabaseDSN, "Data Source Name for database connection (e.g., user:password@tcp(localhost:3306)/dbname)")
+	pflag.String("redis_address", defaultRedisAddress, "Address of the Redis server (e.g., localhost:6379)")
+	pflag.String("redis_password", defaultRedisPassword, "Password for authenticating with the Redis server (if required)")
+	pflag.Int("redis_database", defaultRedisDatabase, "Redis database index to use (default is 1)")
+
+	pflag.String("twilio_sid", defaultTwilioSID, "Password for authenticating with the Redis server (if required)")
+	pflag.String("twilio_token", defaultTwilioToken, "Password for authenticating with the Redis server (if required)")
+	pflag.String("telnyx_connection_id", defaultTelnyxConnectionID, "Password for authenticating with the Redis server (if required)")
+	pflag.String("telnyx_profile_id", defaultTelnyxProfileID, "Password for authenticating with the Redis server (if required)")
+	pflag.String("telnyx_token", defaultTelnyxToken, "Password for authenticating with the Redis server (if required)")
+
+	pflag.Parse()
+
+	// rabbitmq_address
+	if errFlag := viper.BindPFlag("rabbitmq_address", pflag.Lookup("rabbitmq_address")); errFlag != nil {
+		log.Errorf("Error binding flag: %v", errFlag)
+		panic(errFlag)
+	}
+	if errEnv := viper.BindEnv("rabbitmq_address", "RABBITMQ_ADDRESS"); errEnv != nil {
+		log.Errorf("Error binding env: %v", errEnv)
+		panic(errEnv)
+	}
+	rabbitMQAddress = viper.GetString("rabbitmq_address")
+
+	// prometheus_endpoint
+	if errFlag := viper.BindPFlag("prometheus_endpoint", pflag.Lookup("prometheus_endpoint")); errFlag != nil {
+		log.Errorf("Error binding flag: %v", errFlag)
+		panic(errFlag)
+	}
+	if errEnv := viper.BindEnv("prometheus_endpoint", "PROMETHEUS_ENDPOINT"); errEnv != nil {
+		log.Errorf("Error binding env: %v", errEnv)
+		panic(errEnv)
+	}
+	prometheusEndpoint = viper.GetString("prometheus_endpoint")
+
+	// prometheus_listen_address
+	if errFlag := viper.BindPFlag("prometheus_listen_address", pflag.Lookup("prometheus_listen_address")); errFlag != nil {
+		log.Errorf("Error binding flag: %v", errFlag)
+		panic(errFlag)
+	}
+	if errEnv := viper.BindEnv("prometheus_listen_address", "PROMETHEUS_LISTEN_ADDRESS"); errEnv != nil {
+		log.Errorf("Error binding env: %v", errEnv)
+		panic(errEnv)
+	}
+	prometheusListenAddress = viper.GetString("prometheus_listen_address")
+
+	// database_dsn
+	if errFlag := viper.BindPFlag("database_dsn", pflag.Lookup("database_dsn")); errFlag != nil {
+		log.Errorf("Error binding flag: %v", errFlag)
+		panic(errFlag)
+	}
+	if errEnv := viper.BindEnv("database_dsn", "DATABASE_DSN"); errEnv != nil {
+		log.Errorf("Error binding env: %v", errEnv)
+		panic(errEnv)
+	}
+	databaseDSN = viper.GetString("database_dsn")
+
+	// redis_address
+	if errFlag := viper.BindPFlag("redis_address", pflag.Lookup("redis_address")); errFlag != nil {
+		log.Errorf("Error binding flag: %v", errFlag)
+		panic(errFlag)
+	}
+	if errEnv := viper.BindEnv("redis_address", "REDIS_ADDRESS"); errEnv != nil {
+		log.Errorf("Error binding env: %v", errEnv)
+		panic(errEnv)
+	}
+	redisAddress = viper.GetString("redis_address")
+
+	// redis_password
+	if errFlag := viper.BindPFlag("redis_password", pflag.Lookup("redis_password")); errFlag != nil {
+		log.Errorf("Error binding flag: %v", errFlag)
+		panic(errFlag)
+	}
+	if errEnv := viper.BindEnv("redis_password", "REDIS_PASSWORD"); errEnv != nil {
+		log.Errorf("Error binding env: %v", errEnv)
+		panic(errEnv)
+	}
+	redisPassword = viper.GetString("redis_password")
+
+	// redis_database
+	if errFlag := viper.BindPFlag("redis_database", pflag.Lookup("redis_database")); errFlag != nil {
+		log.Errorf("Error binding flag: %v", errFlag)
+		panic(errFlag)
+	}
+	if errEnv := viper.BindEnv("redis_database", "REDIS_DATABASE"); errEnv != nil {
+		log.Errorf("Error binding env: %v", errEnv)
+		panic(errEnv)
+	}
+	redisDatabase = viper.GetInt("redis_database")
+
+	// twilio_sid
+	if errFlag := viper.BindPFlag("twilio_sid", pflag.Lookup("twilio_sid")); errFlag != nil {
+		log.Errorf("Error binding flag: %v", errFlag)
+		panic(errFlag)
+	}
+	if errEnv := viper.BindEnv("twilio_sid", "TWILIO_SID"); errEnv != nil {
+		log.Errorf("Error binding env: %v", errEnv)
+		panic(errEnv)
+	}
+	twilioSID = viper.GetString("twilio_sid")
+
+	// twilio_token
+	if errFlag := viper.BindPFlag("twilio_token", pflag.Lookup("twilio_token")); errFlag != nil {
+		log.Errorf("Error binding flag: %v", errFlag)
+		panic(errFlag)
+	}
+	if errEnv := viper.BindEnv("twilio_token", "TWILIO_TOKEN"); errEnv != nil {
+		log.Errorf("Error binding env: %v", errEnv)
+		panic(errEnv)
+	}
+	twilioToken = viper.GetString("twilio_token")
+
+	// telnyx_connection_id
+	if errFlag := viper.BindPFlag("telnyx_connection_id", pflag.Lookup("telnyx_connection_id")); errFlag != nil {
+		log.Errorf("Error binding flag: %v", errFlag)
+		panic(errFlag)
+	}
+	if errEnv := viper.BindEnv("telnyx_connection_id", "TELNYX_CONNECTION_ID"); errEnv != nil {
+		log.Errorf("Error binding env: %v", errEnv)
+		panic(errEnv)
+	}
+	telnyxConnectionID = viper.GetString("telnyx_connection_id")
+
+	// telnyx_profile_id
+	if errFlag := viper.BindPFlag("telnyx_profile_id", pflag.Lookup("telnyx_profile_id")); errFlag != nil {
+		log.Errorf("Error binding flag: %v", errFlag)
+		panic(errFlag)
+	}
+	if errEnv := viper.BindEnv("telnyx_profile_id", "TELNYX_PROFILE_ID"); errEnv != nil {
+		log.Errorf("Error binding env: %v", errEnv)
+		panic(errEnv)
+	}
+	telnyxProfileID = viper.GetString("telnyx_profile_id")
+
+	// telnyx_token
+	if errFlag := viper.BindPFlag("telnyx_token", pflag.Lookup("telnyx_token")); errFlag != nil {
+		log.Errorf("Error binding flag: %v", errFlag)
+		panic(errFlag)
+	}
+	if errEnv := viper.BindEnv("telnyx_token", "TELNYX_TOKEN"); errEnv != nil {
+		log.Errorf("Error binding env: %v", errEnv)
+		panic(errEnv)
+	}
+	telnyxToken = viper.GetString("telnyx_token")
 }
 
 // signalHandler catches signals and set the done
@@ -139,7 +302,7 @@ func initProm(endpoint, listen string) {
 func run(sqlDB *sql.DB, cache cachehandler.CacheHandler) error {
 
 	// rabbitmq sock connect
-	sockHandler := sockhandler.NewSockHandler(sock.TypeRabbitMQ, *argRabbitAddr)
+	sockHandler := sockhandler.NewSockHandler(sock.TypeRabbitMQ, rabbitMQAddress)
 	sockHandler.Connect()
 
 	// create handlers
@@ -147,8 +310,8 @@ func run(sqlDB *sql.DB, cache cachehandler.CacheHandler) error {
 	reqHandler := requesthandler.NewRequestHandler(sockHandler, serviceName)
 	notifyHandler := notifyhandler.NewNotifyHandler(sockHandler, reqHandler, commonoutline.QueueNameNumberEvent, serviceName)
 
-	nHandlerTelnyx := numberhandlertelnyx.NewNumberHandler(reqHandler, db, *argTelnyxConnectionID, *argTelnyxProfileID, *argTelnyxToken)
-	nHandlerTwilio := numberhandlertwilio.NewNumberHandler(reqHandler, db, *argTwilioSID, *argTwilioToken)
+	nHandlerTelnyx := numberhandlertelnyx.NewNumberHandler(reqHandler, db, telnyxConnectionID, telnyxProfileID, telnyxToken)
+	nHandlerTwilio := numberhandlertwilio.NewNumberHandler(reqHandler, db, twilioSID, twilioToken)
 
 	numberHandler := numberhandler.NewNumberHandler(reqHandler, db, notifyHandler, nHandlerTelnyx, nHandlerTwilio)
 
