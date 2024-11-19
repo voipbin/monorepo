5f9408d2a7e41a437ec7eb4030f514a72c361369
Sungtae Kim <pchero21@gmail.com>
2024-11-20 01:43:13 +0900

    VOIP-932-Fix_wrong_token_validator (#73)
    * Fixed wrong token expiration validation.

--- bin-api-manager/lib/common/common.go
+++ bin-api-manager/lib/common/common.go
@@ -1,19 +1,12 @@
 package common
 
 import (
-	"strings"
 	"time"
+)
+
+const (
+	TokenExpiration = time.Hour * 24 * 7 // 1 week(7 days)
 )
 
 // JSON alias type
 type JSON = map[string]interface{}
-
-// GetCurTime return current utc time string
-func GetCurTime() string {
-	date := time.Now().UTC()
-
-	res := date.String()
-	res = strings.TrimSuffix(res, " +0000 UTC")
-
-	return res
-}
