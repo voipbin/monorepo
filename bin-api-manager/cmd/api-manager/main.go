9150154563138b84e90c36cf6f3f420fd1bd16fc
Sungtae Kim <pchero21@gmail.com>
2024-11-28 03:11:34 +0900

    WIP

--- bin-api-manager/cmd/api-manager/main.go
+++ bin-api-manager/cmd/api-manager/main.go
@@ -218,16 +218,16 @@ func runListen(serviceHandler servicehandler.ServiceHandler) {
 	}
 }
 
-// ping handler
-//
-//	@Summary		Returns message pong
-//	@Description	Used to check the server is alive
-//	@Produce		json
-//	@Router			/ping [get]
-//	@Success		200	"{"message": "pong"}"
-//	@BasePath
-func testPing(c *gin.Context) {
-	c.JSON(200, gin.H{
-		"message": "pong",
-	})
-}
+// // ping handler
+// //
+// //	@Summary		Returns message pong
+// //	@Description	Used to check the server is alive
+// //	@Produce		json
+// //	@Router			/ping [get]
+// //	@Success		200	"{"message": "pong"}"
+// //	@BasePath
+// func testPing(c *gin.Context) {
+// 	c.JSON(200, gin.H{
+// 		"message": "pong",
+// 	})
+// }
