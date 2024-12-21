05b2dec142f8eebf7e932f1346a285d0c154e8f8
Sungtae Kim <pchero21@gmail.com>
2024-12-22 03:35:54 +0900

    WIP

--- bin-common-handler/go.mod
+++ bin-common-handler/go.mod
@@ -63,6 +63,7 @@ require (
 	github.com/pkg/errors v0.9.1
 	github.com/prometheus/client_golang v1.20.5
 	github.com/rabbitmq/amqp091-go v1.10.0
+	github.com/stretchr/testify v1.10.0
 	monorepo/bin-agent-manager v0.0.0-20240328054741-55144017eccd
 	monorepo/bin-billing-manager v0.0.0-20240408051040-600f0028fbab
 	monorepo/bin-call-manager v0.0.0-20240403030948-51eb7c33cf9a
@@ -91,9 +92,14 @@ require (
 require (
 	github.com/beorn7/perks v1.0.1 // indirect
 	github.com/cespare/xxhash/v2 v2.3.0 // indirect
+	github.com/davecgh/go-spew v1.1.1 // indirect
+	github.com/kr/text v0.2.0 // indirect
 	github.com/munnerz/goautoneg v0.0.0-20191010083416-a7dc8b61c822 // indirect
+	github.com/pmezard/go-difflib v1.0.0 // indirect
 	github.com/prometheus/client_model v0.6.1 // indirect
 	github.com/prometheus/common v0.61.0 // indirect
 	github.com/prometheus/procfs v0.15.1 // indirect
-	google.golang.org/protobuf v1.35.2 // indirect
+	github.com/stretchr/objx v0.5.2 // indirect
+	google.golang.org/protobuf v1.36.0 // indirect
+	gopkg.in/yaml.v3 v3.0.1 // indirect
 )
--- bin-common-handler/pkg/rabbitmqhandler/consume.go
+++ bin-common-handler/pkg/rabbitmqhandler/consume.go
@@ -10,6 +10,64 @@ import (
 	"github.com/sirupsen/logrus"
 )
 
+// // ConsumeMessage consumes messages from the given queue with provided options.
+// // If the queueName is not provided, it defaults to a pre-configured queue.
+// // It uses goroutines with a worker pool to process messages concurrently.
+// func (r *rabbit) ConsumeMessage(queueName string, consumerName string, exclusive bool, noLocal bool, noWait bool, numWorkers int, messageConsume sock.CbMsgConsume) error {
+// 	log := logrus.WithFields(logrus.Fields{
+// 		"func":          "ConsumeMessage",
+// 		"queue_name":    queueName,
+// 		"consumer_name": consumerName,
+// 	})
+
+// 	// Get the queue; if not found, return an error.
+// 	queue := r.queueGet(queueName)
+// 	if queue == nil {
+// 		return fmt.Errorf("queue '%s' not found", queueName)
+// 	}
+
+// 	// Create a buffered channel to manage the number of concurrent workers.
+// 	workers := make(chan struct{}, numWorkers)
+
+// 	// Start consuming messages.
+// 	for {
+// 		// Consume messages from the queue.
+// 		messages, err := queue.channel.Consume(
+// 			queueName,    // Queue name
+// 			consumerName, // Consumer name
+// 			false,        // auto-ack (manual acknowledgement)
+// 			exclusive,    // Exclusive (used for binding the queue to the current connection)
+// 			noLocal,      // No-local (only send messages to consumers on the same connection)
+// 			noWait,       // No-wait (do not wait for confirmation)
+// 			nil,          // Additional arguments (nil in this case)
+// 		)
+// 		if err != nil {
+// 			log.Errorf("Failed to consume message from queue '%s': %v", queueName, err)
+// 			return fmt.Errorf("could not consume messages: %v", err)
+// 		}
+
+// 		// Process messages received from the queue.
+// 		for message := range messages {
+// 			workers <- struct{}{} // Block if the max number of workers is reached.
+
+// 			go func(m amqp.Delivery) {
+// 				defer func() { <-workers }()
+
+// 				// note: Acknowledgement should be done before processing the message.
+// 				// otherwise, it will block the channel and the message will not be consumed.
+// 				if err := m.Ack(false); err != nil {
+// 					log.Errorf("Error acknowledging message: %v", err)
+// 				}
+
+// 				if err := r.executeConsumeMessage(m, messageConsume); err != nil {
+// 					log.Errorf("Error while processing message: %v", err)
+// 				}
+
+// 			}(message)
+// 		}
+// 	}
+// }
+
 // ConsumeMessage consumes messages from the given queue with provided options.
 // If the queueName is not provided, it defaults to a pre-configured queue.
 // It uses goroutines with a worker pool to process messages concurrently.
@@ -26,44 +84,41 @@ func (r *rabbit) ConsumeMessage(queueName string, consumerName string, exclusive
 		return fmt.Errorf("queue '%s' not found", queueName)
 	}
 
-	// Create a buffered channel to manage the number of concurrent workers.
-	workers := make(chan struct{}, numWorkers)
-
 	// Start consuming messages.
-	for {
-		// Consume messages from the queue.
-		messages, err := queue.channel.Consume(
-			queueName,    // Queue name
-			consumerName, // Consumer name
-			false,        // auto-ack (manual acknowledgement)
-			exclusive,    // Exclusive (used for binding the queue to the current connection)
-			noLocal,      // No-local (only send messages to consumers on the same connection)
-			noWait,       // No-wait (do not wait for confirmation)
-			nil,          // Additional arguments (nil in this case)
-		)
-		if err != nil {
-			log.Errorf("Failed to consume message from queue '%s': %v", queueName, err)
-			return fmt.Errorf("could not consume messages: %v", err)
+	// Consume messages from the queue.
+	messages, err := queue.channel.Consume(
+		queueName,    // Queue name
+		consumerName, // Consumer name
+		false,        // auto-ack (manual acknowledgement)
+		exclusive,    // Exclusive (used for binding the queue to the current connection)
+		noLocal,      // No-local (only send messages to consumers on the same connection)
+		noWait,       // No-wait (do not wait for confirmation)
+		nil,          // Additional arguments (nil in this case)
+	)
+	if err != nil {
+		log.Errorf("Failed to consume message from queue '%s': %v", queueName, err)
+		return fmt.Errorf("could not consume messages: %v", err)
+	}
+
+	for i := 0; i < numWorkers; i++ {
+		go r.consumeMessageWorker(messages, messageConsume)
+	}
+
+	select {}
+}
+
+func (r *rabbit) consumeMessageWorker(messages <-chan amqp.Delivery, messageConsume sock.CbMsgConsume) {
+	log := logrus.WithFields(logrus.Fields{
+		"func": "consumeMessageWorker",
+	})
+
+	for message := range messages {
+		if err := r.executeConsumeMessage(message, messageConsume); err != nil {
+			log.Errorf("Error while processing message: %v", err)
 		}
 
-		// Process messages received from the queue.
-		for message := range messages {
-			workers <- struct{}{} // Block if the max number of workers is reached.
-
-			go func(m amqp.Delivery) {
-				defer func() { <-workers }()
-
-				// note: Acknowledgement should be done before processing the message.
-				// otherwise, it will block the channel and the message will not be consumed.
-				if err := m.Ack(false); err != nil {
-					log.Errorf("Error acknowledging message: %v", err)
-				}
-
-				if err := r.executeConsumeMessage(m, messageConsume); err != nil {
-					log.Errorf("Error while processing message: %v", err)
-				}
-
-			}(message)
+		if err := message.Ack(false); err != nil {
+			log.Errorf("Error acknowledging message: %v", err)
 		}
 	}
 }
@@ -94,7 +149,7 @@ func (r *rabbit) ConsumeRPC(queueName, consumerName string, exclusive bool, noLo
 
 	workers := make(chan int, numWorkers)
 	for {
-		// fetch teh messages
+		// fetch messages
 		messages, err := queue.channel.Consume(
 			queueName,    // queue
 			consumerName, // messageConsumer
