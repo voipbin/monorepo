4359a6c3ba87450cb1e8d46d558917ffe3c40384
Sungtae Kim <pchero21@gmail.com>
2024-12-24 03:06:07 +0900

    NOJIRA-Fix_wrong_ws_handle (#92)
    * Fixed wrong websocket topic creation

--- bin-api-manager/pkg/subscribehandler/webhookmanager_test.go
+++ bin-api-manager/pkg/subscribehandler/webhookmanager_test.go
@@ -5,9 +5,11 @@ import (
 	"encoding/json"
 	"testing"
 
+	"github.com/gofrs/uuid"
 	gomock "go.uber.org/mock/gomock"
 
 	"monorepo/bin-api-manager/pkg/zmqpubhandler"
+	commonidentity "monorepo/bin-common-handler/models/identity"
 	"monorepo/bin-common-handler/models/sock"
 )
 
@@ -61,7 +63,7 @@ func Test_processEventWebhookManagerWebhookPublished(t *testing.T) {
 			expectEvent: `{"data":{"current_action":{"id":"00000000-0000-0000-0000-000000000001","next_id":"00000000-0000-0000-0000-000000000000","type":""},"customer_id":"5e4a0680-804e-11ec-8477-2fea5968d85b","flow_id":"d78245f0-8239-4298-b27b-1e3fc2185ba0","forward_action_id":"00000000-0000-0000-0000-000000000000","id":"d5d70d9d-85ad-4ffc-90c1-fdda37c046b0","reference_id":"167e552a-a739-473d-b7a7-245b258a4af0","reference_type":"call","tm_create":"2022-07-23 15:02:35.763577","tm_delete":"9999-01-01 00:00:00.000000","tm_update":"2022-07-23 15:02:35.763577"},"type":"activeflow_created"}`,
 		},
 		{
-			name: "agent level normal",
+			name: "owner level normal",
 
 			request: &sock.Event{
 				Type:      "webhook_published",
@@ -72,7 +74,7 @@ func Test_processEventWebhookManagerWebhookPublished(t *testing.T) {
 					  "data": {
 						"id": "7b0966c0-da98-11ee-97df-1786497422fb",
 						"customer_id": "7b6cc58a-da98-11ee-b114-3fb0f4ae9318",
-						"agent_id": "7b3d4468-da98-11ee-a5f0-e32e2e370da3"
+						"owner_id": "7b3d4468-da98-11ee-a5f0-e32e2e370da3"
 					  },
 					  "type": "chatroom_created"
 					},
@@ -85,7 +87,7 @@ func Test_processEventWebhookManagerWebhookPublished(t *testing.T) {
 				"agent_id:7b3d4468-da98-11ee-a5f0-e32e2e370da3:chatroom:7b0966c0-da98-11ee-97df-1786497422fb",
 				"customer_id:7b6cc58a-da98-11ee-b114-3fb0f4ae9318:chatroom:7b0966c0-da98-11ee-97df-1786497422fb",
 			},
-			expectEvent: `{"data":{"agent_id":"7b3d4468-da98-11ee-a5f0-e32e2e370da3","customer_id":"7b6cc58a-da98-11ee-b114-3fb0f4ae9318","id":"7b0966c0-da98-11ee-97df-1786497422fb"},"type":"chatroom_created"}`,
+			expectEvent: `{"data":{"customer_id":"7b6cc58a-da98-11ee-b114-3fb0f4ae9318","id":"7b0966c0-da98-11ee-97df-1786497422fb","owner_id":"7b3d4468-da98-11ee-a5f0-e32e2e370da3"},"type":"chatroom_created"}`,
 		},
 	}
 
@@ -108,6 +110,103 @@ func Test_processEventWebhookManagerWebhookPublished(t *testing.T) {
 
 			if err := h.processEventWebhookManagerWebhookPublished(ctx, tt.request); err != nil {
 				t.Errorf("Wrong match. expect: ok, got: %v", err)
+			}
+		})
+	}
+}
+
+func Test_createTopics(t *testing.T) {
+
+	type test struct {
+		name string
+
+		messageType string
+		data        *commonWebhookData
+
+		expectTopics []string
+		expectError  bool
+	}
+
+	tests := []test{
+		{
+			name:        "have id, customer_id",
+			messageType: "activeflow_created",
+			data: &commonWebhookData{
+				Identity: commonidentity.Identity{
+					ID:         uuid.FromStringOrNil("d5d70d9d-85ad-4ffc-90c1-fdda37c046b0"),
+					CustomerID: uuid.FromStringOrNil("5e4a0680-804e-11ec-8477-2fea5968d85b"),
+				},
+			},
+			expectTopics: []string{
+				"customer_id:5e4a0680-804e-11ec-8477-2fea5968d85b:activeflow:d5d70d9d-85ad-4ffc-90c1-fdda37c046b0",
+			},
+			expectError: false,
+		},
+		{
+			name:        "have id, customer_id, owner_id",
+			messageType: "chatroom_created",
+			data: &commonWebhookData{
+				Identity: commonidentity.Identity{
+					ID:         uuid.FromStringOrNil("7b0966c0-da98-11ee-97df-1786497422fb"),
+					CustomerID: uuid.FromStringOrNil("7b6cc58a-da98-11ee-b114-3fb0f4ae9318"),
+				},
+				Owner: commonidentity.Owner{
+					OwnerID: uuid.FromStringOrNil("7b3d4468-da98-11ee-a5f0-e32e2e370da3"),
+				},
+			},
+			expectTopics: []string{
+				"customer_id:7b6cc58a-da98-11ee-b114-3fb0f4ae9318:chatroom:7b0966c0-da98-11ee-97df-1786497422fb",
+				"agent_id:7b3d4468-da98-11ee-a5f0-e32e2e370da3:chatroom:7b0966c0-da98-11ee-97df-1786497422fb",
+			},
+			expectError: false,
+		},
+		{
+			name:        "have id, owner_id",
+			messageType: "task_created",
+			data: &commonWebhookData{
+				Identity: commonidentity.Identity{
+					ID: uuid.FromStringOrNil("8c0966c0-da98-11ee-97df-1786497422fb"),
+				},
+				Owner: commonidentity.Owner{
+					OwnerID: uuid.FromStringOrNil("8c3d4468-da98-11ee-a5f0-e32e2e370da3"),
+				},
+			},
+			expectTopics: []string{
+				"agent_id:8c3d4468-da98-11ee-a5f0-e32e2e370da3:task:8c0966c0-da98-11ee-97df-1786497422fb",
+			},
+			expectError: false,
+		},
+		{
+			name:        "invalid message type",
+			messageType: "invalidtype",
+			data: &commonWebhookData{
+				Identity: commonidentity.Identity{
+					ID:         uuid.FromStringOrNil("9d0966c0-da98-11ee-97df-1786497422fb"),
+					CustomerID: uuid.FromStringOrNil("9d6cc58a-da98-11ee-b114-3fb0f4ae9318"),
+				},
+			},
+			expectTopics: []string{},
+			expectError:  true,
+		},
+	}
+
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			h := &subscribeHandler{}
+
+			topics, err := h.createTopics(tt.messageType, tt.data)
+			if (err != nil) != tt.expectError {
+				t.Errorf("Unexpected error. expect: %v, got: %v", tt.expectError, err)
+			}
+
+			if len(topics) != len(tt.expectTopics) {
+				t.Errorf("Unexpected number of topics. expect: %d, got: %d", len(tt.expectTopics), len(topics))
+			}
+
+			for i, topic := range topics {
+				if topic != tt.expectTopics[i] {
+					t.Errorf("Unexpected topic. expect: %s, got: %s", tt.expectTopics[i], topic)
+				}
 			}
 		})
 	}
