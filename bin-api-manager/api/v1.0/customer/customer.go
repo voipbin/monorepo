0a9f39ce5af3bc57659d37928b0a7e2e85f80d8f
Sungtae Kim <pchero21@gmail.com>
2024-12-27 22:10:59 +0900

    Fixed

---
+++ bin-api-manager/api/v1.0/customer/customer.go
@@ -0,0 +1,93 @@
+package customer
+
+import (
+	amagent "monorepo/bin-agent-manager/models/agent"
+	"monorepo/bin-api-manager/api/models/common"
+	"monorepo/bin-api-manager/api/models/request"
+	"monorepo/bin-api-manager/pkg/servicehandler"
+
+	"github.com/gin-gonic/gin"
+	"github.com/sirupsen/logrus"
+)
+
+// customerGET handles GET /customer request.
+// It gets the agent.
+//
+//	@Summary		Get the logged in agent
+//	@Description	Get the logged in agent information
+//	@Produce		json
+//	@Param			id	path	string	true	"The ID of the customer"
+//	@Success		200
+//	@Router			/v1.0/customer [get]
+func customerGET(c *gin.Context) {
+	log := logrus.WithFields(logrus.Fields{
+		"func":            "customerGET",
+		"request_address": c.ClientIP,
+	})
+
+	tmp, exists := c.Get("agent")
+	if !exists {
+		log.Errorf("Could not find agent info.")
+		c.AbortWithStatus(400)
+		return
+	}
+	a := tmp.(amagent.Agent)
+	log = log.WithFields(logrus.Fields{
+		"agent":    a,
+		"username": a.Username,
+	})
+
+	serviceHandler := c.MustGet(common.OBJServiceHandler).(servicehandler.ServiceHandler)
+	res, err := serviceHandler.CustomerGet(c.Request.Context(), &a, a.ID)
+	if err != nil {
+		log.Infof("Could not get the customer info. err: %v", err)
+		c.AbortWithStatus(400)
+		return
+	}
+
+	c.JSON(200, res)
+}
+
+// customerPut handles PUT /customer request.
+// It updates a exist customer info with the given customer info.
+// And returns updated customer info if it succeed.
+//
+//	@Summary		Update a customer.
+//	@Description	Update a customer and returns detail updated customer info.
+//	@Produce		json
+//	@Success		200	{object}	customer.Customer
+//	@Router			/v1.0/customer [put]
+func customerPut(c *gin.Context) {
+	log := logrus.WithFields(logrus.Fields{
+		"func":            "customerPut",
+		"request_address": c.ClientIP,
+	})
+
+	tmpAgent, exists := c.Get("agent")
+	if !exists {
+		log.Errorf("Could not find agent info.")
+		c.AbortWithStatus(400)
+		return
+	}
+	a := tmpAgent.(amagent.Agent)
+	log = log.WithFields(logrus.Fields{
+		"agent": a,
+	})
+
+	var req request.BodyCustomersIDPUT
+	if err := c.BindJSON(&req); err != nil {
+		log.Errorf("Could not parse the request. err: %v", err)
+		c.AbortWithStatus(400)
+		return
+	}
+
+	serviceHandler := c.MustGet(common.OBJServiceHandler).(servicehandler.ServiceHandler)
+	res, err := serviceHandler.CustomerUpdate(c.Request.Context(), &a, a.CustomerID, req.Name, req.Detail, req.Email, req.PhoneNumber, req.Address, req.WebhookMethod, req.WebhookURI)
+	if err != nil {
+		log.Errorf("Could not update the customer. err: %v", err)
+		c.AbortWithStatus(400)
+		return
+	}
+
+	c.JSON(200, res)
+}
---
+++ bin-api-manager/api/v1.0/customer/customer_test.go
@@ -0,0 +1,172 @@
+package customer
+
+import (
+	"bytes"
+	"encoding/json"
+	"monorepo/bin-api-manager/api/models/common"
+	"monorepo/bin-api-manager/api/models/request"
+	"monorepo/bin-api-manager/pkg/servicehandler"
+
+	amagent "monorepo/bin-agent-manager/models/agent"
+	commonidentity "monorepo/bin-common-handler/models/identity"
+	cscustomer "monorepo/bin-customer-manager/models/customer"
+
+	"net/http"
+	"net/http/httptest"
+	"testing"
+
+	"github.com/gin-gonic/gin"
+	"github.com/gofrs/uuid"
+	"go.uber.org/mock/gomock"
+)
+
+func setupServer(app *gin.Engine) {
+	v1 := app.RouterGroup.Group("/v1.0")
+	ApplyRoutes(v1)
+}
+
+func Test_customerGET(t *testing.T) {
+
+	tests := []struct {
+		name  string
+		agent amagent.Agent
+
+		reqQuery string
+
+		responseCustomer *cscustomer.WebhookMessage
+
+		expectedCustomerID uuid.UUID
+		expectedRes        string
+	}{
+		{
+			name: "normal",
+			agent: amagent.Agent{
+				Identity: commonidentity.Identity{
+					ID:         uuid.FromStringOrNil("2a2ec0ba-8004-11ec-aea5-439829c92a7c"),
+					CustomerID: uuid.FromStringOrNil("e25f1af8-c44f-11ef-9d46-bfaf61e659c2"),
+				},
+				Permission: amagent.PermissionCustomerAdmin,
+			},
+
+			reqQuery: "/v1.0/customer",
+
+			responseCustomer: &cscustomer.WebhookMessage{
+				ID: uuid.FromStringOrNil("2a2ec0ba-8004-11ec-aea5-439829c92a7c"),
+			},
+
+			expectedCustomerID: uuid.FromStringOrNil("e25f1af8-c44f-11ef-9d46-bfaf61e659c2"),
+			expectedRes:        `{"id":"2a2ec0ba-8004-11ec-aea5-439829c92a7c","billing_account_id":"00000000-0000-0000-0000-000000000000","tm_create":"","tm_update":"","tm_delete":""}`,
+		},
+	}
+
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			mc := gomock.NewController(t)
+			defer mc.Finish()
+
+			mockSvc := servicehandler.NewMockServiceHandler(mc)
+
+			w := httptest.NewRecorder()
+			_, r := gin.CreateTestContext(w)
+
+			r.Use(func(c *gin.Context) {
+				c.Set(common.OBJServiceHandler, mockSvc)
+				c.Set("agent", tt.agent)
+			})
+			setupServer(r)
+
+			req, _ := http.NewRequest("GET", tt.reqQuery, nil)
+			mockSvc.EXPECT().CustomerGet(req.Context(), &tt.agent, tt.agent.ID).Return(tt.responseCustomer, nil)
+
+			r.ServeHTTP(w, req)
+			if w.Code != http.StatusOK {
+				t.Errorf("Wrong match. expect: %d, got: %d", http.StatusOK, w.Code)
+			}
+
+			if w.Body.String() != tt.expectedRes {
+				t.Errorf("Wrong match.\nexpect: %v\ngot: %v", tt.expectedRes, w.Body)
+			}
+		})
+	}
+}
+
+func Test_customerPut(t *testing.T) {
+
+	tests := []struct {
+		name   string
+		agent  amagent.Agent
+		target string
+
+		req request.BodyCustomerPUT
+
+		responseCustomer *cscustomer.WebhookMessage
+
+		expectedCustomerID uuid.UUID
+		expectedRes        string
+	}{
+		{
+			name: "normal",
+			agent: amagent.Agent{
+				Identity: commonidentity.Identity{
+					ID:         uuid.FromStringOrNil("4afd144c-c451-11ef-a8d8-6fd67202355e"),
+					CustomerID: uuid.FromStringOrNil("4b7dcc68-c451-11ef-a289-33cbfe065115"),
+				},
+				Permission: amagent.PermissionCustomerAdmin,
+			},
+			target: "/v1.0/customer",
+
+			req: request.BodyCustomerPUT{
+				Name:          "new name",
+				Detail:        "new detail",
+				Email:         "test@test.com",
+				PhoneNumber:   "+821100000001",
+				Address:       "somewhere",
+				WebhookMethod: cscustomer.WebhookMethodPost,
+				WebhookURI:    "test.com",
+			},
+
+			responseCustomer: &cscustomer.WebhookMessage{
+				ID: uuid.FromStringOrNil("4b7dcc68-c451-11ef-a289-33cbfe065115"),
+			},
+
+			expectedCustomerID: uuid.FromStringOrNil("4b7dcc68-c451-11ef-a289-33cbfe065115"),
+			expectedRes:        `{"id":"4b7dcc68-c451-11ef-a289-33cbfe065115","billing_account_id":"00000000-0000-0000-0000-000000000000","tm_create":"","tm_update":"","tm_delete":""}`,
+		},
+	}
+
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			mc := gomock.NewController(t)
+			defer mc.Finish()
+
+			mockSvc := servicehandler.NewMockServiceHandler(mc)
+
+			w := httptest.NewRecorder()
+			_, r := gin.CreateTestContext(w)
+
+			r.Use(func(c *gin.Context) {
+				c.Set(common.OBJServiceHandler, mockSvc)
+				c.Set("agent", tt.agent)
+			})
+			setupServer(r)
+
+			body, err := json.Marshal(tt.req)
+			if err != nil {
+				t.Errorf("Wong match. expect: ok, got: %v", err)
+			}
+			req, _ := http.NewRequest(http.MethodPut, tt.target, bytes.NewBuffer(body))
+			req.Header.Set("Content-Type", "application/json")
+
+			mockSvc.EXPECT().CustomerUpdate(req.Context(), &tt.agent, tt.expectedCustomerID, tt.req.Name, tt.req.Detail, tt.req.Email, tt.req.PhoneNumber, tt.req.Address, tt.req.WebhookMethod, tt.req.WebhookURI).Return(tt.responseCustomer, nil)
+
+			r.ServeHTTP(w, req)
+			if w.Code != http.StatusOK {
+				t.Errorf("Wrong match. expect: %d, got: %d", http.StatusOK, w.Code)
+			}
+
+			if w.Body.String() != tt.expectedRes {
+				t.Errorf("Wrong match.\nexpect: %v\ngot: %v", tt.expectedRes, w.Body)
+			}
+		})
+	}
+}
