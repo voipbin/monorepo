a6a452752622851f49900be8930a67cf46be5bc6
Sungtae Kim <pchero21@gmail.com>
2024-12-31 23:17:06 +0900

    Fixed

--- voip-asterisk-proxy/cmd/asterisk-proxy/main.go
+++ voip-asterisk-proxy/cmd/asterisk-proxy/main.go
@@ -2,9 +2,9 @@ package main
 
 import (
 	"context"
-	"flag"
 	"fmt"
 	"net"
+	"os"
 	"strings"
 	"time"
 
@@ -14,7 +14,6 @@ import (
 
 	"github.com/go-redis/redis/v8"
 	"github.com/ivahaev/amigo"
-	joonix "github.com/joonix/log"
 	"github.com/sirupsen/logrus"
 
 	commonoutline "monorepo/bin-common-handler/models/outline"
@@ -22,53 +21,61 @@ import (
 	"monorepo/voip-asterisk-proxy/pkg/listenhandler"
 )
 
-const serviceName = "asterisk-proxy"
+const (
+	serviceName = "asterisk-proxy"
+)
 
-var flagARIAddr = flag.String("ari_addr", "localhost:8088", "The asterisk-proxy connects to this asterisk ari service address")
-var flagARIAccount = flag.String("ari_account", "asterisk:asterisk", "The asterisk-proxy uses this asterisk ari account info. id:password")
-var flagARISubscribeAll = flag.String("ari_subscribe_all", "true", "The asterisk-proxy uses this asterisk subscribe all option.")
-var flagARIApplication = flag.String("ari_application", "voipbin", "The asterisk-proxy uses this asterisk ari application name.")
+var (
+	ariAddr         = ""
+	ariAccount      = ""
+	ariSubscribeAll = ""
+	ariApplication  = ""
 
-var flagAMIHost = flag.String("ami_host", "127.0.0.1", "The host address for AMI connection.")
-var flagAMIPort = flag.String("ami_port", "5038", "The port number for AMI connection.")
-var flagAMIUsername = flag.String("ami_username", "asterisk", "The username for AMI login.")
-var flagAMIPassword = flag.String("ami_password", "asterisk", "The password for AMI login.")
-var flagAMIEventFilter = flag.String("ami_event_filter", "", "The list of messages for listen.")
+	amiHost        = ""
+	amiPort        = ""
+	amiUsername    = ""
+	amiPassword    = ""
+	amiEventFilter = ""
 
-var flagInterfaceName = flag.String("interface_name", "eth0", "The main interface device name.")
+	interfaceName = ""
 
-var flagRabbitAddr = flag.String("rabbit_addr", "amqp://guest:guest@localhost:5672", "The asterisk-proxy connect to rabbitmq address.")
-var flagRabbitQueueListenRequest = flag.String("rabbit_queue_listen", "asterisk.call.request", "Additional comma separated asterisk-proxy's listen request queue name.")
-var flagRabbitQueuePublishEvent = flag.String("rabbit_queue_publish", "asterisk.all.event", "The asterisk-proxy sends the ARI event to this rabbitmq queue name. The queue must be created before.")
+	rabbitAddr         = ""
+	rabbitQueueListen  = ""
+	rabbitQueuePublish = ""
 
-var flagRedisAddr = flag.String("redis_addr", "localhost:6379", "The redis address for data caching")
-var flagRedisDB = flag.Int("redis_db", 0, "The redis database for caching")
+	redisAddr = ""
+	redisDB   = 0
+)
+
+var chSigs = make(chan os.Signal, 1)
 
 func main() {
+	log := logrus.WithField("func", "main")
+
 	initProcess()
 
 	// connect to rabbitmq
-	logrus.Debugf("rabbitmq address: %s", *flagRabbitAddr)
-	rabbitSock := rabbitmqhandler.NewRabbit(*flagRabbitAddr)
+	log.Debugf("rabbitmq address: %s", rabbitAddr)
+	rabbitSock := rabbitmqhandler.NewRabbit(rabbitAddr)
 	rabbitSock.Connect()
 
 	// connect to ami
-	amiSock := connectAMI(*flagAMIHost, *flagAMIPort, *flagAMIUsername, *flagAMIPassword)
+	amiSock := connectAMI(amiHost, amiPort, amiUsername, amiPassword)
 
 	// create a filter
-	amiFilter := strings.Split(*flagAMIEventFilter, ",")
+	amiFilter := strings.Split(amiEventFilter, ",")
 
 	// get asterisk id and internal address
-	asteriskID, asteriskAddress, err := getAsteriskIDAddress(*flagInterfaceName)
+	asteriskID, asteriskAddress, err := getAsteriskIDAddress(interfaceName)
 	if err != nil {
-		logrus.Errorf("Could not get correct asterisk-id, asterisk-address info.")
+		log.Errorf("Could not get correct asterisk-id, asterisk-address info.")
 		return
 	}
 
 	// create rabbitmq listen requet queue names
-	rabbitQueueListenRequestsPermanent := *flagRabbitQueueListenRequest
+	rabbitQueueListenRequestsPermanent := rabbitQueueListen
 	rabbitQueueListenRequestsVolatile := fmt.Sprintf("asterisk.%s.request", asteriskID)
-	logrus.Debugf("Volatile listen queue name: %s", rabbitQueueListenRequestsVolatile)
+	log.Debugf("Volatile listen queue name: %s", rabbitQueueListenRequestsVolatile)
 
 	reqHandler := requesthandler.NewRequestHandler(rabbitSock, serviceName)
 	notifyHandler := notifyhandler.NewNotifyHandler(rabbitSock, reqHandler, commonoutline.QueueNameAsteriskEventAll, serviceName)
@@ -77,58 +84,47 @@ func main() {
 	evtHandler := eventhandler.NewEventHandler(
 		notifyHandler,
 		rabbitSock,
-		*flagRabbitQueuePublishEvent,
-		*flagARIAddr,
-		*flagARIAccount,
-		*flagARISubscribeAll,
-		*flagARIApplication,
+		rabbitQueuePublish,
+		ariAddr,
+		ariAccount,
+		ariSubscribeAll,
+		ariApplication,
 		amiSock,
 		amiFilter,
 	)
 
 	// run event handler
 	if err := evtHandler.Run(); err != nil {
-		logrus.Errorf("Could not run the eventhandler correctly. err: %v", err)
+		log.Errorf("Could not run the eventhandler correctly. err: %v", err)
 		return
 	}
-	logrus.Debugf("The event handler is running now. listen: %s", rabbitQueueListenRequestsPermanent)
+	log.Debugf("The event handler is running now. listen: %s", rabbitQueueListenRequestsPermanent)
 
 	// create a listen handler
 	listenHandler := listenhandler.NewListenHandler(
 		rabbitSock,
 		rabbitQueueListenRequestsPermanent,
 		rabbitQueueListenRequestsVolatile,
-		*flagARIAddr,
-		*flagARIAccount,
+		ariAddr,
+		ariAccount,
 		amiSock,
 	)
 
 	// run listen handler
 	if err := listenHandler.Run(); err != nil {
-		logrus.Errorf("Could not run the listen handler correctly. err: %v", err)
+		log.Errorf("Could not run the listen handler correctly. err: %v", err)
 		return
 	}
-	logrus.Debugf("The listen handler is running now. id: %s, address: %v", asteriskID, asteriskAddress)
+	log.Debugf("The listen handler is running now. id: %s, address: %v", asteriskID, asteriskAddress)
 
-	if err := handleProxyInfo(*flagRedisAddr, *flagRedisDB, asteriskID, asteriskAddress); err != nil {
-		logrus.Errorf("Could not initiate proxy info handler. err: %v", err)
+	if err := handleProxyInfo(redisAddr, redisDB, asteriskID, asteriskAddress); err != nil {
+		log.Errorf("Could not initiate proxy info handler. err: %v", err)
 		return
 	}
 
-	forever := make(chan bool)
-	<-forever
-}
+	sig := <-chSigs
+	log.Infof("Terminating api-manager. sig: %v", sig)
 
-// initProcess inits process
-func initProcess() {
-	// initiate flags
-	flag.Parse()
-
-	// initiate log
-	logrus.SetFormatter(joonix.NewFormatter())
-	logrus.SetLevel(logrus.DebugLevel)
-
-	logrus.Info("asterisk-proxy has initiated.")
 }
 
 // connectAMI access and login to the Asterisk's AMI.
