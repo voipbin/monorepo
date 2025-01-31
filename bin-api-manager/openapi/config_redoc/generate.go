a9e549a23f41d283fad8a116a5d39f91109e3068
Sungtae Kim <pchero21@gmail.com>
2025-01-31 20:22:37 +0900

    VOIP-975-Add_openapi_manager (#108)
    * Added bin-openapi-manager

--- bin-api-manager/openapi/config_redoc/generate.go
+++ bin-api-manager/openapi/config_redoc/generate.go
@@ -1,3 +1,3 @@
 package generate
 
-//go:generate sh -c "mkdir -p ../../gens/openapi_redoc &&  command -v npx >/dev/null 2>&1 && npx @redocly/cli build-docs ../openapi.yaml --output ../../gens/openapi_redoc/api.html || echo 'npx is not installed, skipping generation'"
+//go:generate sh -c "mkdir -p ../../gens/openapi_redoc &&  command -v npx >/dev/null 2>&1 && npx @redocly/cli build-docs ../../../bin-openapi-manager/openapi/openapi.yaml --output ../../gens/openapi_redoc/api.html || echo 'npx is not installed, skipping generation'"
