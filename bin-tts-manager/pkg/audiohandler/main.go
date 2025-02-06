ac4055a638143c3c114fefa846fc88dcb1c5b775
Sungtae Kim <pchero21@gmail.com>
2025-02-07 01:16:59 +0900

    WIP

--- bin-tts-manager/pkg/audiohandler/main.go
+++ bin-tts-manager/pkg/audiohandler/main.go
@@ -7,12 +7,16 @@ import (
 	"encoding/base64"
 	"io/fs"
 	"log"
+	"time"
 
 	texttospeech "cloud.google.com/go/texttospeech/apiv1"
 	texttospeechpb "cloud.google.com/go/texttospeech/apiv1/texttospeechpb"
 	"github.com/gofrs/uuid"
 	"github.com/sirupsen/logrus"
 	"google.golang.org/api/option"
+	"google.golang.org/grpc"
+	"google.golang.org/grpc/credentials/insecure"
+	"google.golang.org/grpc/keepalive"
 
 	"monorepo/bin-tts-manager/models/tts"
 )
@@ -38,6 +42,21 @@ const (
 func NewAudioHandler(credentialBase64 string) AudioHandler {
 	ctx := context.Background()
 
+	keepAliveParams := keepalive.ClientParameters{
+		Time:                30 * time.Second, // Ping every 30 seconds
+		Timeout:             10 * time.Second, // Wait 10 seconds for response
+		PermitWithoutStream: true,             // Send pings even if there are no active streams
+	}
+
+	conn, err := grpc.NewClient(
+		"texttospeech.googleapis.com:443",
+		grpc.WithTransportCredentials(insecure.NewCredentials()), // Use default SSL/TLS
+		grpc.WithKeepaliveParams(keepAliveParams),
+	)
+	if err != nil {
+		log.Fatalf("Failed to create gRPC connection: %v", err)
+	}
+
 	decodedCredential, err := base64.StdEncoding.DecodeString(credentialBase64)
 	if err != nil {
 		log.Printf("Error decoding base64 credential: %v", err)
@@ -45,7 +64,7 @@ func NewAudioHandler(credentialBase64 string) AudioHandler {
 	}
 
 	// create client
-	client, err := texttospeech.NewClient(ctx, option.WithCredentialsJSON(decodedCredential))
+	client, err := texttospeech.NewClient(ctx, option.WithGRPCConn(conn), option.WithCredentialsJSON(decodedCredential))
 	if err != nil {
 		logrus.Errorf("Could not create a new client. err: %v", err)
 		return nil
