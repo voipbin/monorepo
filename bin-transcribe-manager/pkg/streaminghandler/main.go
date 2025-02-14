9a0233a471972e62321873ce88b489d10f261e53
Sungtae Kim <pchero21@gmail.com>
2025-02-14 21:35:20 +0900

    WIP

--- bin-transcribe-manager/pkg/streaminghandler/main.go
+++ bin-transcribe-manager/pkg/streaminghandler/main.go
@@ -45,7 +45,7 @@ const (
 	defaultTransport      = string(cmexternalmedia.TransportTCP)
 	defaultConnectionType = "client"
 	// defaultFormat         = "ulaw"
-	defaultFormat = "slin16"
+	defaultFormat = "slin"
 )
 
 // default gcp stt options
