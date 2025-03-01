be93136947ae602570766e9f26728539ed951eb7
Sungtae Kim <pchero21@gmail.com>
2025-03-02 05:33:25 +0900

    VOIP-1000-Refactor_and_validate_database_scheme (#136)
    * Fixed readme and wrong database scheme.

--- bin-dbscheme-bin-manager/bin-manager/main/versions/e8b72d3bf93e_add_index_calls.py
+++ bin-dbscheme-bin-manager/bin-manager/main/versions/e8b72d3bf93e_add_index_calls.py
@@ -18,9 +18,7 @@ depends_on = None
 
 def upgrade():
     op.execute("""create index idx_calls_customerid_and_tmcreate on calls(customer_id, tm_create);""")
-    op.execute("""alter table calls add column flow_id binary(16) after bridge_id;""")
 
 
 def downgrade():
     op.execute("""alter table calls drop index idx_calls_customerid_and_tmcreate;""")
-    op.execute("""alter table calls drop column flow_id;""")
