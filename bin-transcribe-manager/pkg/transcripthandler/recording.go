cf3810d87d8eea1b86ff4acb7dac6ea4fdd62f8a
Sungtae Kim <pchero21@gmail.com>
2025-03-28 00:24:45 +0900

    Fixed

--- bin-transcribe-manager/pkg/transcripthandler/recording.go
+++ bin-transcribe-manager/pkg/transcripthandler/recording.go
@@ -4,6 +4,8 @@ import (
 	"context"
 	"fmt"
 	"regexp"
+	"sort"
+	"time"
 
 	"github.com/gofrs/uuid"
 	"github.com/pkg/errors"
@@ -31,6 +33,7 @@ func (h *transcriptHandler) Recording(ctx context.Context, customerID uuid.UUID,
 	}
 	log.WithField("files", files).Debugf("Got the files. recording_id: %s", recordingID)
 
+	tmpTranscripts := []*transcript.Transcript{}
 	for _, file := range files {
 
 		direction := parseDirection(file.Filename)
@@ -41,18 +44,24 @@ func (h *transcriptHandler) Recording(ctx context.Context, customerID uuid.UUID,
 			return nil, errors.Wrapf(err, "could not transcribe the recording. recording_id: %s", recordingID)
 		}
 		log.Debugf("Transcripted the recording. transcribe_id: %s, len: %d", transcribeID, len(tmps))
-
-		for _, tmp := range tmps {
-			t, err := h.Create(ctx, customerID, transcribeID, direction, tmp.Message, tmp.TMTranscript)
-			if err != nil {
-				// we could not create transcript here, but we should not return an error
-				log.Errorf("Could not create a tracript. message: %s, err: %v", tmp.Message, err)
-			}
-			log.WithField("transcript", t).Debugf("Created a new transcript. transcript_id: %s, transcribe_id: %s", t.ID, t.TranscribeID)
-		}
+		tmpTranscripts = append(tmpTranscripts, tmps...)
 	}
 
-	return nil, fmt.Errorf("test Error")
+	// sort by tm_transcript
+	res := []*transcript.Transcript{}
+	sortTranscriptsByTMTranscript(tmpTranscripts)
+	for _, tmp := range tmpTranscripts {
+		t, err := h.Create(ctx, customerID, transcribeID, tmp.Direction, tmp.Message, tmp.TMTranscript)
+		if err != nil {
+			// we could not create transcript here, but we should not return an error
+			log.Errorf("Could not create a tracript. message: %s, err: %v", tmp.Message, err)
+		}
+		res = append(res, t)
+
+		log.WithField("transcript", t).Debugf("Created a new transcript. transcript_id: %s, transcribe_id: %s", t.ID, t.TranscribeID)
+	}
+
+	return res, nil
 }
 
 func parseDirection(filename string) transcript.Direction {
@@ -72,4 +81,20 @@ func parseDirection(filename string) transcript.Direction {
 	default:
 		return transcript.DirectionBoth
 	}
+}
+
+func sortTranscriptsByTMTranscript(transcripts []*transcript.Transcript) {
+	sort.SliceStable(transcripts, func(i, j int) bool {
+		// Parse TMTranscript to time.Time
+		t1, err1 := time.Parse("2006-01-02 15:04:05.000000", transcripts[i].TMTranscript)
+		t2, err2 := time.Parse("2006-01-02 15:04:05.000000", transcripts[j].TMTranscript)
+
+		// Handle invalid date parsing (if needed, can return false or use a fallback)
+		if err1 != nil || err2 != nil {
+			return false
+		}
+
+		// Compare the times
+		return t1.Before(t2)
+	})
 }
