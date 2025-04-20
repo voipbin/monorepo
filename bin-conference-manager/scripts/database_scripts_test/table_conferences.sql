eb579441645e5dc1f6b171758c34fe19fbe83439
Sungtae Kim <pchero21@gmail.com>
2025-04-21 04:55:51 +0900

    Fixed

--- bin-conference-manager/scripts/database_scripts_test/table_conferences.sql
+++ bin-conference-manager/scripts/database_scripts_test/table_conferences.sql
@@ -16,9 +16,6 @@ create table conference_conferences(
   pre_flow_id  binary(16),   -- pre flow id
   post_flow_id binary(16),   -- post flow id
 
-  pre_actions   json,     -- action set for before conference join(enter)
-  post_actions  json,     -- action set for after conference join(enter)
-
   conferencecall_ids json, -- conferencecalls in the conference
 
   -- record info
