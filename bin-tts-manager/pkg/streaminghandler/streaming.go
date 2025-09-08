e4d183ff8e72454d3281029fa866e631b5044f93
Sungtae Kim <pchero21@gmail.com>
2025-09-09 02:28:18 +0900

    Fixed

--- bin-tts-manager/pkg/streaminghandler/streaming.go
+++ bin-tts-manager/pkg/streaminghandler/streaming.go
@@ -7,7 +7,6 @@ import (
 	"sync"
 
 	"github.com/gofrs/uuid"
-	"github.com/sirupsen/logrus"
 
 	commonidentity "monorepo/bin-common-handler/models/identity"
 	"monorepo/bin-tts-manager/models/streaming"
@@ -111,15 +110,9 @@ func (h *streamingHandler) UpdateConnAst(streamingID uuid.UUID, connAst net.Conn
 }
 
 func (h *streamingHandler) SetVendorInfo(st *streaming.Streaming, venderName streaming.VendorName, vendorConfig any) {
-	log := logrus.WithFields(logrus.Fields{
-		"func":        "SetVendorInfo",
-		"streaming":   st,
-		"vendor_name": venderName,
-	})
 	st.VendorLock.Lock()
 	defer st.VendorLock.Unlock()
 
-	log.Debugf("Setting vendor info. vendor_name: %s", venderName)
 	st.VendorName = venderName
 	st.VendorConfig = vendorConfig
 }
