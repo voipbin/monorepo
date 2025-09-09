be8a59cdb89ee83409819ccf73477bb95b7b37ee
Sungtae Kim <pchero21@gmail.com>
2025-09-10 02:19:16 +0900

    WIP

--- bin-ai-manager/pkg/engine_openai_handler/tool.go
+++ bin-ai-manager/pkg/engine_openai_handler/tool.go
@@ -5,11 +5,37 @@ import "github.com/sashabaranov/go-openai"
 var (
 	tools = []openai.Tool{
 		toolConnect,
-		toolEmailSend,
+		// toolEmailSend,
 	}
 )
 
 var (
+	// toolConnect = openai.Tool{
+	// 	Type: openai.ToolTypeFunction,
+	// 	Function: &openai.FunctionDefinition{
+	// 		Name:        "connect",
+	// 		Description: "creates a new call to the destinations and connects to them",
+	// 		Parameters: map[string]any{
+	// 			"type": "object",
+	// 			"properties": map[string]any{
+	// 				"source": map[string]any{
+	// 					"type":        "one of agent/conference/email/extension/line/sip/tel",
+	// 					"target":      "address endpoint",
+	// 					"target_name": "address's name",
+	// 				},
+	// 				"destinations": []map[string]any{
+	// 					{
+	// 						"type":        "one of agent/conference/email/extension/line/sip/tel",
+	// 						"target":      "address endpoint",
+	// 						"target_name": "address's name",
+	// 					},
+	// 				},
+	// 			},
+	// 			"required": []string{"destinations"},
+	// 		},
+	// 	},
+	// }
+
 	toolConnect = openai.Tool{
 		Type: openai.ToolTypeFunction,
 		Function: &openai.FunctionDefinition{
@@ -19,15 +45,42 @@ var (
 				"type": "object",
 				"properties": map[string]any{
 					"source": map[string]any{
-						"type":        "one of agent/conference/email/extension/line/sip/tel",
-						"target":      "address endpoint",
-						"target_name": "address's name",
+						"type": "object",
+						"properties": map[string]any{
+							"type": map[string]any{
+								"type":        "string",
+								"description": "one of agent/conference/email/extension/line/sip/tel",
+							},
+							"target": map[string]any{
+								"type":        "string",
+								"description": "address endpoint",
+							},
+							"target_name": map[string]any{
+								"type":        "string",
+								"description": "address's name",
+							},
+						},
+						"required": []string{"type", "target"},
 					},
-					"destinations": []map[string]any{
-						{
-							"type":        "one of agent/conference/email/extension/line/sip/tel",
-							"target":      "address endpoint",
-							"target_name": "address's name",
+					"destinations": map[string]any{
+						"type": "array",
+						"items": map[string]any{
+							"type": "object",
+							"properties": map[string]any{
+								"type": map[string]any{
+									"type":        "string",
+									"description": "one of agent/conference/email/extension/line/sip/tel",
+								},
+								"target": map[string]any{
+									"type":        "string",
+									"description": "address endpoint",
+								},
+								"target_name": map[string]any{
+									"type":        "string",
+									"description": "address's name",
+								},
+							},
+							"required": []string{"type", "target"},
 						},
 					},
 				},
@@ -36,37 +89,37 @@ var (
 		},
 	}
 
-	toolEmailSend = openai.Tool{
-		Type: openai.ToolTypeFunction,
-		Function: &openai.FunctionDefinition{
-			Name:        "email_send",
-			Description: "sends the email.",
-			Parameters: map[string]any{
-				"type": "object",
-				"properties": map[string]any{
-					"source": map[string]any{
-						"type":        "one of agent/conference/email/extension/line/sip/tel",
-						"target":      "address endpoint",
-						"target_name": "address's name",
-					},
-					"destinations": []map[string]any{
-						{
-							"type":        "one of agent/conference/email/extension/line/sip/tel",
-							"target":      "address endpoint",
-							"target_name": "address's name",
-						},
-					},
-					"subject": "email subject",
-					"content": "email content",
-					"attachments": []map[string]any{
-						{
-							"reference_type": "reference type of attachment resource",
-							"reference_id":   "reference id of attachment resource",
-						},
-					},
-				},
-				"required": []string{"destinations"},
-			},
-		},
-	}
+	// toolEmailSend = openai.Tool{
+	// 	Type: openai.ToolTypeFunction,
+	// 	Function: &openai.FunctionDefinition{
+	// 		Name:        "email_send",
+	// 		Description: "sends the email.",
+	// 		Parameters: map[string]any{
+	// 			"type": "object",
+	// 			"properties": map[string]any{
+	// 				"source": map[string]any{
+	// 					"type":        "one of agent/conference/email/extension/line/sip/tel",
+	// 					"target":      "address endpoint",
+	// 					"target_name": "address's name",
+	// 				},
+	// 				"destinations": []map[string]any{
+	// 					{
+	// 						"type":        "one of agent/conference/email/extension/line/sip/tel",
+	// 						"target":      "address endpoint",
+	// 						"target_name": "address's name",
+	// 					},
+	// 				},
+	// 				"subject": "email subject",
+	// 				"content": "email content",
+	// 				"attachments": []map[string]any{
+	// 					{
+	// 						"reference_type": "reference type of attachment resource",
+	// 						"reference_id":   "reference id of attachment resource",
+	// 					},
+	// 				},
+	// 			},
+	// 			"required": []string{"destinations"},
+	// 		},
+	// 	},
+	// }
 )
