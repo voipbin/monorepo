6906731650b5d5d0e4a688c2c633977f43d4295c
Sungtae Kim <pchero21@gmail.com>
2025-09-12 03:48:16 +0900

    WIP

--- bin-ai-manager/pkg/engine_openai_handler/tool.go
+++ bin-ai-manager/pkg/engine_openai_handler/tool.go
@@ -14,37 +14,10 @@ import (
 var (
 	tools = []openai.Tool{
 		toolConnect,
-		// toolEmailSend,
 	}
 )
 
 var (
-	// toolConnect = openai.Tool{
-	// 	Type: openai.ToolTypeFunction,
-	// 	Function: &openai.FunctionDefinition{
-	// 		Name:        "connect",
-	// 		Description: "creates a new call to the destinations and connects to them",
-	// 		Parameters: map[string]any{
-	// 			"type": "object",
-	// 			"properties": map[string]any{
-	// 				"source": map[string]any{
-	// 					"type":        "one of agent/conference/email/extension/line/sip/tel",
-	// 					"target":      "address endpoint",
-	// 					"target_name": "address's name",
-	// 				},
-	// 				"destinations": []map[string]any{
-	// 					{
-	// 						"type":        "one of agent/conference/email/extension/line/sip/tel",
-	// 						"target":      "address endpoint",
-	// 						"target_name": "address's name",
-	// 					},
-	// 				},
-	// 			},
-	// 			"required": []string{"destinations"},
-	// 		},
-	// 	},
-	// }
-
 	toolConnect = openai.Tool{
 		Type: openai.ToolTypeFunction,
 		Function: &openai.FunctionDefinition{
@@ -97,40 +70,6 @@ var (
 			},
 		},
 	}
-
-	// toolEmailSend = openai.Tool{
-	// 	Type: openai.ToolTypeFunction,
-	// 	Function: &openai.FunctionDefinition{
-	// 		Name:        "email_send",
-	// 		Description: "sends the email.",
-	// 		Parameters: map[string]any{
-	// 			"type": "object",
-	// 			"properties": map[string]any{
-	// 				"source": map[string]any{
-	// 					"type":        "one of agent/conference/email/extension/line/sip/tel",
-	// 					"target":      "address endpoint",
-	// 					"target_name": "address's name",
-	// 				},
-	// 				"destinations": []map[string]any{
-	// 					{
-	// 						"type":        "one of agent/conference/email/extension/line/sip/tel",
-	// 						"target":      "address endpoint",
-	// 						"target_name": "address's name",
-	// 					},
-	// 				},
-	// 				"subject": "email subject",
-	// 				"content": "email content",
-	// 				"attachments": []map[string]any{
-	// 					{
-	// 						"reference_type": "reference type of attachment resource",
-	// 						"reference_id":   "reference id of attachment resource",
-	// 					},
-	// 				},
-	// 			},
-	// 			"required": []string{"destinations"},
-	// 		},
-	// 	},
-	// }
 )
 
 func (h *engineOpenaiHandler) toolHandle(actionName string, actionOption []byte) (*fmaction.Action, error) {
