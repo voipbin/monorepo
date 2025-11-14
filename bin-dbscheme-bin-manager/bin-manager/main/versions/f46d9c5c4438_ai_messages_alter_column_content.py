b187d430129d8d9cb372133f7fdfffae715dd157
Sungtae Kim <pchero21@gmail.com>
2025-11-15 05:12:04 +0900

    VOIP-1128-Change_column_utf8mb4 (#271)
    * bin-dbscheme-bin-manager: Updates the content column character set from utf8mb3 to utf8mb4 with utf8mb4_general_ci collation
    * bin-dbscheme-bin-manager: Provides a downgrade path to revert to utf8mb3 if needed

---
+++ bin-dbscheme-bin-manager/bin-manager/main/versions/f46d9c5c4438_ai_messages_alter_column_content.py
@@ -0,0 +1,34 @@
+"""ai_messages alter column content
+
+Revision ID: f46d9c5c4438
+Revises: bad27b40fe8e
+Create Date: 2025-11-15 04:56:30.216182
+
+"""
+from alembic import op
+import sqlalchemy as sa
+
+
+# revision identifiers, used by Alembic.
+revision = 'f46d9c5c4438'
+down_revision = 'bad27b40fe8e'
+branch_labels = None
+depends_on = None
+
+
+def upgrade():
+    op.execute("""
+        ALTER TABLE ai_messages 
+        MODIFY COLUMN content TEXT 
+        CHARACTER SET utf8mb4 
+        COLLATE utf8mb4_general_ci;
+    """)
+
+
+def downgrade():
+    op.execute("""
+        ALTER TABLE ai_messages 
+        MODIFY COLUMN content TEXT 
+        CHARACTER SET utf8mb3 
+        COLLATE utf8mb3_general_ci;
+    """)
