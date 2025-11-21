a0b68807e4c0a9f4e92bf04928340351f37f1fed
Sungtae Kim <pchero21@gmail.com>
2025-11-21 12:51:08 +0900

    WIP

--- bin-pipecat-manager/pkg/pipecatcallhandler/pythonrunner.go
+++ bin-pipecat-manager/pkg/pipecatcallhandler/pythonrunner.go
@@ -3,9 +3,7 @@ package pipecatcallhandler
 //go:generate mockgen -package pipecatcallhandler -destination ./mock_pythonrunner.go -source pythonrunner.go -build_flags=-mod=mod
 
 import (
-	"bytes"
 	"context"
-	"encoding/json"
 	"io"
 	"net/http"
 
@@ -50,59 +48,59 @@ func (h *pythonRunner) Start(
 	voiceID string,
 	messages []map[string]any,
 ) error {
-	log := logrus.WithFields(logrus.Fields{
-		"func": "Start",
-	})
+	// log := logrus.WithFields(logrus.Fields{
+	// 	"func": "Start",
+	// })
 
-	// // only used to send data to the python runner
-	reqBody := struct {
-		ID       uuid.UUID        `json:"id,omitempty"`
-		LLMType  string           `json:"llm_type,omitempty"`
-		LLMKey   string           `json:"llm_key,omitempty"`
-		TTS      string           `json:"tts,omitempty"`
-		STT      string           `json:"stt,omitempty"`
-		VoiceID  string           `json:"voice_id,omitempty"`
-		Messages []map[string]any `json:"messages,omitempty"`
-	}{
-		ID:       pipecatcallID,
-		LLMType:  llmType,
-		LLMKey:   llmKey,
-		STT:      stt,
-		TTS:      tts,
-		VoiceID:  voiceID,
-		Messages: messages,
-	}
+	// // // only used to send data to the python runner
+	// reqBody := struct {
+	// 	ID       uuid.UUID        `json:"id,omitempty"`
+	// 	LLMType  string           `json:"llm_type,omitempty"`
+	// 	LLMKey   string           `json:"llm_key,omitempty"`
+	// 	TTS      string           `json:"tts,omitempty"`
+	// 	STT      string           `json:"stt,omitempty"`
+	// 	VoiceID  string           `json:"voice_id,omitempty"`
+	// 	Messages []map[string]any `json:"messages,omitempty"`
+	// }{
+	// 	ID:       pipecatcallID,
+	// 	LLMType:  llmType,
+	// 	LLMKey:   llmKey,
+	// 	STT:      stt,
+	// 	TTS:      tts,
+	// 	VoiceID:  voiceID,
+	// 	Messages: messages,
+	// }
 
-	jsonData, err := json.Marshal(reqBody)
-	if err != nil {
-		return errors.Wrapf(err, "could not marshal request body for python runner")
-	}
+	// jsonData, err := json.Marshal(reqBody)
+	// if err != nil {
+	// 	return errors.Wrapf(err, "could not marshal request body for python runner")
+	// }
 
-	url := defaultPipecatRunnerListenAddress + "/run"
-	req, err := http.NewRequestWithContext(ctx, "POST", url, bytes.NewBuffer(jsonData))
-	if err != nil {
-		return errors.Wrapf(err, "could not create request for python runner")
-	}
-	req.Header.Set("Content-Type", "application/json")
+	// url := defaultPipecatRunnerListenAddress + "/run"
+	// req, err := http.NewRequestWithContext(ctx, "POST", url, bytes.NewBuffer(jsonData))
+	// if err != nil {
+	// 	return errors.Wrapf(err, "could not create request for python runner")
+	// }
+	// req.Header.Set("Content-Type", "application/json")
 
-	log.WithField("request_body", string(jsonData)).Debugf("Sending request to python runner")
-	client := &http.Client{}
-	resp, err := client.Do(req)
-	if err != nil {
-		return errors.Wrapf(err, "could not send request to python runner")
-	}
-	defer func() {
-		_ = resp.Body.Close()
-	}()
+	// log.WithField("request_body", string(jsonData)).Debugf("Sending request to python runner")
+	// client := &http.Client{}
+	// resp, err := client.Do(req)
+	// if err != nil {
+	// 	return errors.Wrapf(err, "could not send request to python runner")
+	// }
+	// defer func() {
+	// 	_ = resp.Body.Close()
+	// }()
 
-	body, err := io.ReadAll(resp.Body)
-	if err != nil {
-		return errors.Wrapf(err, "could not read response body from python runner")
-	}
+	// body, err := io.ReadAll(resp.Body)
+	// if err != nil {
+	// 	return errors.Wrapf(err, "could not read response body from python runner")
+	// }
 
-	if resp.StatusCode != http.StatusOK {
-		return errors.Errorf("python runner returned non-200 status: %s, body: %s", resp.Status, string(body))
-	}
+	// if resp.StatusCode != http.StatusOK {
+	// 	return errors.Errorf("python runner returned non-200 status: %s, body: %s", resp.Status, string(body))
+	// }
 
 	return nil
 }
