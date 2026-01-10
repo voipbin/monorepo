e03bf977b42f8eff0829b3b0f0a37ef815c79d2a
Sungtae Kim <pchero21@gmail.com>
2026-01-10 22:27:55 +0900

    refactor: Migrate bin-outdial-manager, bin-pipecat-manager, and bin-queue-manager to Cobra/Viper
    Standardize configuration management across three services:
    
    bin-outdial-manager:
    - Create internal/config package with Bootstrap and Get functions
    - Replace init.go flag binding with Cobra command structure
    - Add proper signal handling and Prometheus initialization
    - Update main.go to use config.Get() for all configuration access
    
    bin-pipecat-manager:
    - Create internal/config package with Bootstrap and Get functions
    - Replace init.go flag binding with Cobra command structure
    - Add proper signal handling and Prometheus initialization
    - Update main.go and run() to use config.Get()
    
    bin-queue-manager:
    - Create internal/config package with Bootstrap and Get functions
    - Replace init.go flag binding with Cobra command structure
    - Add proper signal handling and Prometheus initialization
    - Update main.go and run() to use config.Get()
    
    All services:
    - Improve error handling with wrapped errors
    - All tests pass and linting clean
    - Updated vendor dependencies

--- bin-queue-manager/cmd/queue-manager/main.go
+++ bin-queue-manager/cmd/queue-manager/main.go
@@ -1,7 +1,10 @@
 package main
 
 import (
+	"net/http"
 	"os"
+	"os/signal"
+	"syscall"
 
 	commonoutline "monorepo/bin-common-handler/models/outline"
 	"monorepo/bin-common-handler/models/sock"
@@ -12,8 +15,12 @@ import (
 	"monorepo/bin-common-handler/pkg/sockhandler"
 
 	_ "github.com/go-sql-driver/mysql"
+	"github.com/pkg/errors"
+	"github.com/prometheus/client_golang/prometheus/promhttp"
 	"github.com/sirupsen/logrus"
+	"github.com/spf13/cobra"
 
+	"monorepo/bin-queue-manager/internal/config"
 	"monorepo/bin-queue-manager/pkg/cachehandler"
 	"monorepo/bin-queue-manager/pkg/dbhandler"
 	"monorepo/bin-queue-manager/pkg/listenhandler"
@@ -28,47 +35,73 @@ const serviceName = commonoutline.ServiceNameQueueManager
 var chSigs = make(chan os.Signal, 1)
 var chDone = make(chan bool, 1)
 
-var (
-	databaseDSN             = ""
-	prometheusEndpoint      = ""
-	prometheusListenAddress = ""
-	rabbitMQAddress         = ""
-	redisAddress            = ""
-	redisDatabase           = 0
-	redisPassword           = ""
-)
-
 func main() {
-	log := logrus.WithField("func", "main")
+	rootCmd := &cobra.Command{
+		Use:   "queue-manager",
+		Short: "Voipbin Queue Manager Daemon",
+		RunE: func(cmd *cobra.Command, args []string) error {
+			return runDaemon()
+		},
+	}
+
+	if errBind := config.Bootstrap(rootCmd); errBind != nil {
+		logrus.Fatalf("Failed to bind config: %v", errBind)
+	}
+
+	if errExecute := rootCmd.Execute(); errExecute != nil {
+		logrus.Errorf("Command execution failed: %v", errExecute)
+		os.Exit(1)
+	}
+}
+
+func runDaemon() error {
+	initSignal()
+	initProm(config.Get().PrometheusEndpoint, config.Get().PrometheusListenAddress)
+
+	log := logrus.WithField("func", "runDaemon")
+	log.WithField("config", config.Get()).Info("Starting queue-manager...")
 
 	// connect to database
-	sqlDB, err := commondatabasehandler.Connect(databaseDSN)
+	sqlDB, err := commondatabasehandler.Connect(config.Get().DatabaseDSN)
 	if err != nil {
-		log.Errorf("Could not connect to database. err: %v", err)
-		return
+		return errors.Wrapf(err, "could not connect to database")
 	}
 	defer commondatabasehandler.Close(sqlDB)
 
 	// connect to cache
-	cache := cachehandler.NewHandler(redisAddress, redisPassword, redisDatabase)
+	cache := cachehandler.NewHandler(config.Get().RedisAddress, config.Get().RedisPassword, config.Get().RedisDatabase)
 	if err := cache.Connect(); err != nil {
-		log.Errorf("Could not connect to cache server. err: %v", err)
-		return
+		return errors.Wrapf(err, "could not connect to cache server")
 	}
 
 	db := dbhandler.NewHandler(sqlDB, cache)
 
-	if err := run(db); err != nil {
-		log.Errorf("Run func has finished. err: %v", err)
+	if errRun := run(db); errRun != nil {
+		return errors.Wrapf(errRun, "run func has finished")
 	}
+
 	<-chDone
+	log.Info("Queue-manager stopped safely.")
+	return nil
 }
 
-// signalHandler catches signals and set the done
-func signalHandler() {
-	sig := <-chSigs
-	logrus.Debugf("Received signal. sig: %v", sig)
-	chDone <- true
+func initSignal() {
+	signal.Notify(chSigs, syscall.SIGINT, syscall.SIGTERM, syscall.SIGHUP)
+	go func() {
+		sig := <-chSigs
+		logrus.Infof("Received signal: %v", sig)
+		chDone <- true
+	}()
+}
+
+func initProm(endpoint, listen string) {
+	http.Handle(endpoint, promhttp.Handler())
+	go func() {
+		logrus.Infof("Prometheus metrics server starting on %s%s", listen, endpoint)
+		if err := http.ListenAndServe(listen, nil); err != nil {
+			logrus.Errorf("Prometheus server error: %v", err)
+		}
+	}()
 }
 
 // run runs the listen
@@ -80,7 +113,7 @@ func run(db dbhandler.DBHandler) error {
 	)
 
 	// rabbitmq sock connect
-	sockHandler := sockhandler.NewSockHandler(sock.TypeRabbitMQ, rabbitMQAddress)
+	sockHandler := sockhandler.NewSockHandler(sock.TypeRabbitMQ, config.Get().RabbitMQAddress)
 	sockHandler.Connect()
 
 	// create handlers
