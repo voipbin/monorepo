aee112d1df3ec67dd813350dd1b76c9f9041a5fd
Sungtae Kim <pchero21@gmail.com>
2026-01-15 17:15:24 +0900

    feat(registrar-control): add handler initialization functions

--- bin-registrar-manager/cmd/registrar-control/main.go
+++ bin-registrar-manager/cmd/registrar-control/main.go
@@ -1,16 +1,44 @@
 package main
 
 import (
+	"context"
+	"database/sql"
+	"encoding/json"
+	"fmt"
 	"log"
 
+	"monorepo/bin-registrar-manager/internal/config"
+	"monorepo/bin-registrar-manager/pkg/cachehandler"
+	"monorepo/bin-registrar-manager/pkg/dbhandler"
+	"monorepo/bin-registrar-manager/pkg/extensionhandler"
+	"monorepo/bin-registrar-manager/pkg/trunkhandler"
+	commonoutline "monorepo/bin-common-handler/models/outline"
+	"monorepo/bin-common-handler/models/sock"
+	commondatabasehandler "monorepo/bin-common-handler/pkg/databasehandler"
+	"monorepo/bin-common-handler/pkg/notifyhandler"
+	"monorepo/bin-common-handler/pkg/requesthandler"
+	"monorepo/bin-common-handler/pkg/sockhandler"
+
+	"github.com/AlecAivazis/survey/v2"
+	"github.com/gofrs/uuid"
+	"github.com/pkg/errors"
+	"github.com/sirupsen/logrus"
 	"github.com/spf13/cobra"
 	"github.com/spf13/viper"
-	"github.com/pkg/errors"
-
-	"monorepo/bin-registrar-manager/internal/config"
 )
 
 const serviceName = "registrar-control"
+
+// Suppress unused import errors - these will be used in later tasks
+var (
+	_ = context.Background
+	_ = sql.Open
+	_ = json.Marshal
+	_ = fmt.Sprintf
+	_ = (*survey.Input)(nil)
+	_ = uuid.NewV4
+	_ = logrus.Info
+)
 
 func main() {
 	cmd := initCommand()
@@ -40,4 +68,62 @@ func initCommand() *cobra.Command {
 	cmdRoot.AddCommand(cmdTrunk)
 
 	return cmdRoot
+}
+
+func initCache() (cachehandler.CacheHandler, error) {
+	res := cachehandler.NewHandler(config.Get().RedisAddress, config.Get().RedisPassword, config.Get().RedisDatabase)
+	if err := res.Connect(); err != nil {
+		return nil, errors.Wrap(err, "could not connect to cache")
+	}
+	return res, nil
+}
+
+func initExtensionHandler() (extensionhandler.ExtensionHandler, error) {
+	sqlBin, err := commondatabasehandler.Connect(config.Get().DatabaseDSNBin)
+	if err != nil {
+		return nil, errors.Wrap(err, "could not connect to bin database")
+	}
+
+	sqlAsterisk, err := commondatabasehandler.Connect(config.Get().DatabaseDSNAsterisk)
+	if err != nil {
+		return nil, errors.Wrap(err, "could not connect to asterisk database")
+	}
+
+	cache, err := initCache()
+	if err != nil {
+		return nil, errors.Wrap(err, "could not initialize cache")
+	}
+
+	dbAst := dbhandler.NewHandler(sqlAsterisk, cache)
+	dbBin := dbhandler.NewHandler(sqlBin, cache)
+
+	sockHandler := sockhandler.NewSockHandler(sock.TypeRabbitMQ, config.Get().RabbitMQAddress)
+	sockHandler.Connect()
+
+	reqHandler := requesthandler.NewRequestHandler(sockHandler, serviceName)
+	notifyHandler := notifyhandler.NewNotifyHandler(sockHandler, reqHandler, commonoutline.QueueNameRegistrarEvent, serviceName)
+
+	return extensionhandler.NewExtensionHandler(reqHandler, dbAst, dbBin, notifyHandler), nil
+}
+
+func initTrunkHandler() (trunkhandler.TrunkHandler, error) {
+	sqlBin, err := commondatabasehandler.Connect(config.Get().DatabaseDSNBin)
+	if err != nil {
+		return nil, errors.Wrap(err, "could not connect to bin database")
+	}
+
+	cache, err := initCache()
+	if err != nil {
+		return nil, errors.Wrap(err, "could not initialize cache")
+	}
+
+	dbBin := dbhandler.NewHandler(sqlBin, cache)
+
+	sockHandler := sockhandler.NewSockHandler(sock.TypeRabbitMQ, config.Get().RabbitMQAddress)
+	sockHandler.Connect()
+
+	reqHandler := requesthandler.NewRequestHandler(sockHandler, serviceName)
+	notifyHandler := notifyhandler.NewNotifyHandler(sockHandler, reqHandler, commonoutline.QueueNameRegistrarEvent, serviceName)
+
+	return trunkhandler.NewTrunkHandler(reqHandler, dbBin, notifyHandler), nil
 }
