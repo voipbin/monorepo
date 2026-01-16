d5372c62b54d84c252ae2a21eb1af89c871de0a2
Sungtae Kim <pchero21@gmail.com>
2026-01-17 08:44:04 +0900

    NOJIRA-Add-talk-manager
    - bin-talk-manager: Add talk business logic handler with create, get, list, and delete operations
    - bin-talk-manager: Add webhook support with ConvertWebhookMessage and CreateWebhookEvent methods
    - bin-talk-manager: Add event type constants (talk_created, talk_deleted) for webhook notifications
    - bin-talk-manager: Add all monorepo replace directives to go.mod for dependency resolution
    - bin-talk-manager: Generate mocks for TalkHandler interface using mockgen

--- bin-talk-manager/models/talk/talk.go
+++ bin-talk-manager/models/talk/talk.go
@@ -1,6 +1,8 @@
 package talk
 
 import (
+	"encoding/json"
+
 	commonidentity "monorepo/bin-common-handler/models/identity"
 )
 
@@ -28,8 +30,31 @@ type Talk struct {
 type WebhookMessage struct {
 	commonidentity.Identity
 
-	Type     Type   `json:"type"`
-	TMCreate string `json:"tm_create"`
-	TMUpdate string `json:"tm_update"`
-	TMDelete string `json:"tm_delete"`
+	Type     Type   `json:"type,omitempty"`
+	TMCreate string `json:"tm_create,omitempty"`
+	TMUpdate string `json:"tm_update,omitempty"`
+	TMDelete string `json:"tm_delete,omitempty"`
+}
+
+// ConvertWebhookMessage converts Talk to WebhookMessage
+func (t *Talk) ConvertWebhookMessage() *WebhookMessage {
+	return &WebhookMessage{
+		Identity: t.Identity,
+		Type:     t.Type,
+		TMCreate: t.TMCreate,
+		TMUpdate: t.TMUpdate,
+		TMDelete: t.TMDelete,
+	}
+}
+
+// CreateWebhookEvent generates WebhookEvent JSON
+func (t *Talk) CreateWebhookEvent() ([]byte, error) {
+	e := t.ConvertWebhookMessage()
+
+	m, err := json.Marshal(e)
+	if err != nil {
+		return nil, err
+	}
+
+	return m, nil
 }
---
+++ bin-talk-manager/pkg/talkhandler/talk.go
@@ -0,0 +1,115 @@
+package talkhandler
+
+import (
+	"context"
+
+	"github.com/gofrs/uuid"
+	"github.com/pkg/errors"
+	"github.com/sirupsen/logrus"
+
+	commonidentity "monorepo/bin-common-handler/models/identity"
+
+	"monorepo/bin-talk-manager/models/talk"
+)
+
+// TalkCreate creates a new talk
+func (h *talkHandler) TalkCreate(ctx context.Context, customerID uuid.UUID, talkType talk.Type) (*talk.Talk, error) {
+	log := logrus.WithFields(logrus.Fields{
+		"func":        "TalkCreate",
+		"customer_id": customerID,
+		"type":        talkType,
+	})
+	log.Debug("Creating a new talk")
+
+	// Generate UUID for talk
+	id, err := uuid.NewV4()
+	if err != nil {
+		log.Errorf("Failed to generate UUID. err: %v", err)
+		return nil, errors.Wrap(err, "failed to generate UUID")
+	}
+
+	// Create talk object
+	t := &talk.Talk{
+		Identity: commonidentity.Identity{
+			ID:         id,
+			CustomerID: customerID,
+		},
+		Type: talkType,
+	}
+
+	// Save to database
+	err = h.dbHandler.TalkCreate(ctx, t)
+	if err != nil {
+		log.Errorf("Failed to create talk in database. err: %v", err)
+		return nil, errors.Wrap(err, "failed to create talk in database")
+	}
+
+	// Publish webhook event
+	h.notifyHandler.PublishWebhookEvent(ctx, t.CustomerID, talk.EventTypeTalkCreated, t)
+
+	log.WithField("talk_id", t.ID).Debug("Talk created successfully")
+	return t, nil
+}
+
+// TalkGet retrieves a talk by ID
+func (h *talkHandler) TalkGet(ctx context.Context, id uuid.UUID) (*talk.Talk, error) {
+	log := logrus.WithFields(logrus.Fields{
+		"func":    "TalkGet",
+		"talk_id": id,
+	})
+
+	t, err := h.dbHandler.TalkGet(ctx, id)
+	if err != nil {
+		log.Errorf("Failed to get talk. err: %v", err)
+		return nil, errors.Wrap(err, "failed to get talk")
+	}
+
+	return t, nil
+}
+
+// TalkList retrieves talks with filters and pagination
+func (h *talkHandler) TalkList(ctx context.Context, filters map[talk.Field]any, token string, size uint64) ([]*talk.Talk, error) {
+	log := logrus.WithFields(logrus.Fields{
+		"func":    "TalkList",
+		"filters": filters,
+		"token":   token,
+		"size":    size,
+	})
+
+	talks, err := h.dbHandler.TalkList(ctx, filters, token, size)
+	if err != nil {
+		log.Errorf("Failed to list talks. err: %v", err)
+		return nil, errors.Wrap(err, "failed to list talks")
+	}
+
+	return talks, nil
+}
+
+// TalkDelete soft deletes a talk
+func (h *talkHandler) TalkDelete(ctx context.Context, id uuid.UUID) error {
+	log := logrus.WithFields(logrus.Fields{
+		"func":    "TalkDelete",
+		"talk_id": id,
+	})
+	log.Debug("Deleting talk")
+
+	// Get talk before deletion for webhook
+	t, err := h.dbHandler.TalkGet(ctx, id)
+	if err != nil {
+		log.Errorf("Failed to get talk before deletion. err: %v", err)
+		return errors.Wrap(err, "failed to get talk before deletion")
+	}
+
+	// Soft delete in database
+	err = h.dbHandler.TalkDelete(ctx, id)
+	if err != nil {
+		log.Errorf("Failed to delete talk. err: %v", err)
+		return errors.Wrap(err, "failed to delete talk")
+	}
+
+	// Publish webhook event
+	h.notifyHandler.PublishWebhookEvent(ctx, t.CustomerID, talk.EventTypeTalkDeleted, t)
+
+	log.Debug("Talk deleted successfully")
+	return nil
+}
