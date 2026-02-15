dfa2d1bcd1b869b4a5cf1f5fbb8eb9199a0ba94c
Sungtae Kim <pchero21@gmail.com>
2026-02-16 01:58:48 +0900

    NOJIRA-Headless-signup-auto-provisioning
    - bin-customer-manager: Add POST /v1/customers/complete_signup RPC route
    - bin-customer-manager: Add V1DataCustomersCompleteSignupPost request model

---
+++ bin-customer-manager/pkg/listenhandler/v1_customers_complete_signup.go
@@ -0,0 +1,48 @@
+package listenhandler
+
+import (
+	"context"
+	"encoding/json"
+
+	"monorepo/bin-common-handler/models/sock"
+
+	"github.com/sirupsen/logrus"
+
+	"monorepo/bin-customer-manager/pkg/listenhandler/models/request"
+)
+
+// processV1CustomersCompleteSignupPost handles POST /v1/customers/complete_signup request
+func (h *listenHandler) processV1CustomersCompleteSignupPost(ctx context.Context, m *sock.Request) (*sock.Response, error) {
+	log := logrus.WithFields(logrus.Fields{
+		"func":    "processV1CustomersCompleteSignupPost",
+		"request": m,
+	})
+	log.Debug("Executing processV1CustomersCompleteSignupPost.")
+
+	var reqData request.V1DataCustomersCompleteSignupPost
+	if err := json.Unmarshal([]byte(m.Data), &reqData); err != nil {
+		log.Debugf("Could not unmarshal the data. data: %v, err: %v", m.Data, err)
+		return simpleResponse(400), nil
+	}
+
+	tmp, err := h.customerHandler.CompleteSignup(ctx, reqData.TempToken, reqData.Code)
+	if err != nil {
+		log.Errorf("Could not complete signup. err: %v", err)
+		return simpleResponse(400), nil
+	}
+
+	data, err := json.Marshal(tmp)
+	if err != nil {
+		log.Debugf("Could not marshal the result data. data: %v, err: %v", tmp, err)
+		return simpleResponse(500), nil
+	}
+	log.Debugf("Sending result: %v", data)
+
+	res := &sock.Response{
+		StatusCode: 200,
+		DataType:   "application/json",
+		Data:       data,
+	}
+
+	return res, nil
+}
