0a8bc76d6317cab5205ac79e19dba1ac764f7c4e
Sungtae Kim <pchero21@gmail.com>
2026-03-09 03:13:50 +0900

    NOJIRA-Add-rtpengine-requesthandler-methods
    Improve test coverage for RTPEngineV1CommandsSend.
    
    - bin-common-handler: Assert full sock.Request content instead of gomock.Any()
    - bin-common-handler: Add error test cases (RPC error, 500 status code)
    - bin-common-handler: Replace duplicate test case with different rtpengineID test

--- bin-common-handler/pkg/requesthandler/rtpengine_commands_test.go
+++ bin-common-handler/pkg/requesthandler/rtpengine_commands_test.go
@@ -2,6 +2,7 @@ package requesthandler
 
 import (
 	"context"
+	"fmt"
 	"testing"
 
 	"go.uber.org/mock/gomock"
@@ -17,42 +18,72 @@ func Test_RTPEngineV1CommandsSend(t *testing.T) {
 		rtpengineID string
 		command     map[string]interface{}
 
-		expectQueue  string
-		expectURI    string
-		expectMethod sock.RequestMethod
+		expectQueue string
+		expectData  []byte
 
-		response    *sock.Response
-		expectError bool
+		response      *sock.Response
+		responseError error
+		expectError   bool
 	}{
 		{
 			"start recording",
 			"10.164.0.12",
 			map[string]interface{}{
-				"command": "start recording",
 				"call-id": "abc123@sip-server",
+				"command": "start recording",
 			},
 
 			"rtpengine.10.164.0.12.request",
-			"/v1/commands",
-			sock.RequestMethodPost,
+			[]byte(`{"call-id":"abc123@sip-server","command":"start recording"}`),
 
 			&sock.Response{StatusCode: 200, Data: []byte(`{"result":"ok"}`)},
+			nil,
 			false,
 		},
 		{
-			"stop recording",
+			"different rtpengine instance",
+			"10.164.0.99",
+			map[string]interface{}{
+				"call-id": "def456@sip-server",
+				"command": "stop recording",
+			},
+
+			"rtpengine.10.164.0.99.request",
+			[]byte(`{"call-id":"def456@sip-server","command":"stop recording"}`),
+
+			&sock.Response{StatusCode: 200, Data: []byte(`{"result":"ok"}`)},
+			nil,
+			false,
+		},
+		{
+			"rpc error",
 			"10.164.0.12",
 			map[string]interface{}{
-				"command": "stop recording",
 				"call-id": "abc123@sip-server",
+				"command": "query",
 			},
 
 			"rtpengine.10.164.0.12.request",
-			"/v1/commands",
-			sock.RequestMethodPost,
+			[]byte(`{"call-id":"abc123@sip-server","command":"query"}`),
 
-			&sock.Response{StatusCode: 200, Data: []byte(`{"result":"ok"}`)},
-			false,
+			nil,
+			fmt.Errorf("connection refused"),
+			true,
+		},
+		{
+			"error response status code",
+			"10.164.0.12",
+			map[string]interface{}{
+				"call-id": "abc123@sip-server",
+				"command": "start recording",
+			},
+
+			"rtpengine.10.164.0.12.request",
+			[]byte(`{"call-id":"abc123@sip-server","command":"start recording"}`),
+
+			&sock.Response{StatusCode: 500, Data: []byte(`{"result":"error","error-reason":"Unknown call-id"}`)},
+			nil,
+			true,
 		},
 	}
 
@@ -69,8 +100,13 @@ func Test_RTPEngineV1CommandsSend(t *testing.T) {
 			mockSock.EXPECT().RequestPublish(
 				gomock.Any(),
 				tt.expectQueue,
-				gomock.Any(),
-			).Return(tt.response, nil)
+				&sock.Request{
+					URI:      "/v1/commands",
+					Method:   sock.RequestMethodPost,
+					DataType: "application/json",
+					Data:     tt.expectData,
+				},
+			).Return(tt.response, tt.responseError)
 
 			res, err := reqHandler.RTPEngineV1CommandsSend(context.Background(), tt.rtpengineID, tt.command)
 			if tt.expectError {
