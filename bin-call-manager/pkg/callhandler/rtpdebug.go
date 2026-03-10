6d213451cd497d9a0deb72c9366335a777d95d1b
Sungtae Kim <pchero21@gmail.com>
2026-03-10 16:45:25 +0900

    NOJIRA-add-from-tag-to-start-recording
    Include from-tag in RTPEngine start/stop recording commands to enable
    bidirectional RTP capture. Without from-tag, RTPEngine only records one
    direction of the call.
    
    - bin-call-manager: Add SIPDataKeyFromTag to channel SIPData keys
    - bin-call-manager: Include from-tag parameter in start-recording command
    - bin-call-manager: Include from-tag parameter in stop-recording command
    - bin-call-manager: Update rtp debug test with from_tag in SIPData

--- bin-call-manager/pkg/callhandler/rtpdebug.go
+++ bin-call-manager/pkg/callhandler/rtpdebug.go
@@ -23,9 +23,16 @@ func (h *callHandler) rtpDebugStartRecording(ctx context.Context, cn *channel.Ch
 		return
 	}
 
+	fromTag := cn.SIPData[channel.SIPDataKeyFromTag]
+	if fromTag == "" {
+		log.Debugf("No from_tag in SIPData. Skipping RTP debug start.")
+		return
+	}
+
 	command := map[string]interface{}{
-		"command": "start recording",
-		"call-id": cn.SIPCallID,
+		"command":  "start recording",
+		"call-id":  cn.SIPCallID,
+		"from-tag": fromTag,
 	}
 
 	res, err := h.reqHandler.RTPEngineV1CommandsSend(ctx, rtpengineAddress, command)
@@ -58,9 +65,16 @@ func (h *callHandler) rtpDebugStopRecording(ctx context.Context, c *call.Call) {
 		return
 	}
 
+	fromTag := cn.SIPData[channel.SIPDataKeyFromTag]
+	if fromTag == "" {
+		log.Debugf("No from_tag in SIPData. Skipping RTP debug stop.")
+		return
+	}
+
 	command := map[string]interface{}{
-		"command": "stop recording",
-		"call-id": cn.SIPCallID,
+		"command":  "stop recording",
+		"call-id":  cn.SIPCallID,
+		"from-tag": fromTag,
 	}
 
 	res, err := h.reqHandler.RTPEngineV1CommandsSend(ctx, rtpengineAddress, command)
