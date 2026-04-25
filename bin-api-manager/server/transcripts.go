54c30401887b45dbf50158c091dfe7958401a932
Sungtae Kim <pchero21@gmail.com>
2026-04-25 22:30:42 +0900

    NOJIRA-api-error-pr8-voice-group
    - bin-api-manager: Migrate transcripts handler (1 handler, 2 sites) to canonical error envelope

--- bin-api-manager/server/transcripts.go
+++ bin-api-manager/server/transcripts.go
@@ -2,6 +2,8 @@ package server
 
 import (
 	"monorepo/bin-api-manager/gens/openapi_server"
+	cerrors "monorepo/bin-common-handler/models/errors"
+	commonoutline "monorepo/bin-common-handler/models/outline"
 
 	"github.com/gin-gonic/gin"
 	"github.com/gofrs/uuid"
@@ -17,7 +19,7 @@ func (h *server) GetTranscripts(c *gin.Context, params openapi_server.GetTranscr
 	a, ok := getAuthIdentity(c)
 	if !ok {
 		log.Errorf("Could not find auth identity.")
-		c.AbortWithStatus(400)
+		abortWithError(c, cerrors.Unauthenticated(commonoutline.ServiceNameAPIManager, "AUTHENTICATION_REQUIRED", "Authentication is required."))
 		return
 	}
 	log = log.WithFields(logrus.Fields{
@@ -44,13 +46,15 @@ func (h *server) GetTranscripts(c *gin.Context, params openapi_server.GetTranscr
 	tmps, err := h.serviceHandler.TranscriptList(c.Request.Context(), a, transcribeID)
 	if err != nil {
 		logrus.Errorf("Could not get transcribes info. err: %v", err)
-		c.AbortWithStatus(400)
+		abortWithServiceError(c, err)
 		return
 	}
 
 	nextToken := ""
 	if len(tmps) > 0 {
-		if tmps[len(tmps)-1].TMCreate != nil { nextToken = tmps[len(tmps)-1].TMCreate.UTC().Format("2006-01-02T15:04:05.000000Z") }
+		if tmps[len(tmps)-1].TMCreate != nil {
+			nextToken = tmps[len(tmps)-1].TMCreate.UTC().Format("2006-01-02T15:04:05.000000Z")
+		}
 	}
 
 	res := GenerateListResponse(tmps, nextToken)
